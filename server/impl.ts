9fcf2d831fc70a0210c77caa0c5d8835c9ab1a5d
Philihp Busby <philihp@gmail.com>
2022-11-26 16:20:43 +0000

    hathora init

---
+++ server/impl.ts
@@ -0,0 +1,32 @@
+import { Methods, Context } from "./.hathora/methods";
+import { Response } from "../api/base";
+import {
+  GameStatus,
+  Player,
+  State,
+  UserId,
+  IInitializeRequest,
+  ICreateGameRequest,
+  IMakeMoveRequest,
+} from "../api/types";
+
+type InternalState = State;
+
+export class Impl implements Methods<InternalState> {
+  initialize(ctx: Context, request: IInitializeRequest): InternalState {
+    return {
+      players: [],
+      activePlayer: undefined,
+      moves: [],
+    };
+  }
+  createGame(state: InternalState, userId: UserId, ctx: Context, request: ICreateGameRequest): Response {
+    return Response.error("Not implemented");
+  }
+  makeMove(state: InternalState, userId: UserId, ctx: Context, request: IMakeMoveRequest): Response {
+    return Response.error("Not implemented");
+  }
+  getUserState(state: InternalState, userId: UserId): State {
+    return state;
+  }
+}
