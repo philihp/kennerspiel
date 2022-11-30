8afa229725dc495334721a71cdf1ec23d8e16a39
Philihp Busby <philihp@gmail.com>
2022-11-30 17:23:40 +0000

    clear game statuses

--- server/impl.ts
+++ server/impl.ts
@@ -18,6 +18,7 @@ type InternalState = State
 export class Impl implements Methods<InternalState> {
   initialize(ctx: Context, request: IInitializeRequest): InternalState {
     return {
+      status: GameStatus.SEATING,
       players: [],
       activePlayer: undefined,
       moves: [],
@@ -25,19 +26,44 @@ export class Impl implements Methods<InternalState> {
   }
 
   createGame(state: InternalState, userId: UserId, ctx: Context, request: ICreateGameRequest): Response {
-    return Response.error('Not implemented')
+    if (state.status === GameStatus.STARTED) return Response.error('Game currently in progress. Finish it first')
+    state.status = GameStatus.SEATING
+    state.players = []
+    state.activePlayer = undefined
+    state.moves = []
+    return Response.ok()
   }
 
   joinGame(state: State, userId: string, ctx: Context, request: IJoinGameRequest): Response {
-    return Response.error('Not implemented')
+    if (state.players.find((p) => p.id === userId)) {
+      return Response.error('Already joined')
+    }
+    if (state.status === GameStatus.STARTED) {
+      return Response.error('Game has started')
+    }
+    if (!state.players.some((player: Player) => player.color === request.color)) {
+      state.players.push({ id: userId, color: request.color })
+    }
+    return Response.ok()
   }
 
   endGame(state: State, userId: string, ctx: Context, request: IEndGameRequest): Response {
-    return Response.error('Not implemented')
+    if (state.status === GameStatus.SEATING) {
+      return Response.error('Game has not started yet.')
+    }
+    state.status = GameStatus.FINISHED
+    return Response.ok()
   }
 
   startGame(state: State, userId: string, ctx: Context, request: IStartGameRequest): Response {
-    return Response.error('Not implemented')
+    if (state.status === GameStatus.STARTED) {
+      return Response.error('Game has already started')
+    }
+    if (state.status === GameStatus.FINISHED) {
+      return Response.error('Game has already finished')
+    }
+    state.status = GameStatus.STARTED
+    return Response.ok()
   }
 
   makeMove(state: InternalState, userId: UserId, ctx: Context, request: IMakeMoveRequest): Response {
