440ccc52cc29a2f03545a191a49798beefff9c05
Philihp Busby <philihp@gmail.com>
2023-01-05 23:23:30 +0000

    add a test for costForBuilding

--- game/src/board/__tests__/buildings.test.ts
+++ game/src/board/__tests__/buildings.test.ts
@@ -1,7 +1,12 @@
-import { SettlementRound } from '../../types'
-import { roundBuildings } from '../buildings'
+import { BuildingEnum, SettlementRound } from '../../types'
+import { costForBuilding, roundBuildings } from '../buildings'
 
 describe('build/buildings', () => {
+  describe('costForBuilding', () => {
+    it('has appropriate building materials for Priory', () => {
+      expect(costForBuilding(BuildingEnum.Priory)).toStrictEqual({ clay: 1, wood: 1 })
+    })
+  })
   describe('roundBuildings', () => {
     it('returns a list of buildings', () => {
       expect(roundBuildings({ players: 3, country: 'france', length: 'long' }, SettlementRound.B)).toStrictEqual([
