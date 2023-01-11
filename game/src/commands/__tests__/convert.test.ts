72750863420badd6e82486834fe1202f4b4024f9
Philihp Busby <philihp@gmail.com>
2023-01-11 04:38:48 +0000

    test conversion of wine and whiskey

--- game/src/commands/__tests__/convert.test.ts
+++ game/src/commands/__tests__/convert.test.ts
@@ -57,6 +57,46 @@ describe('commands/convert', () => {
         wine: 1,
       })
     })
+    it('converts wine to coins', () => {
+      const s3 = {
+        ...s2,
+        players: [
+          {
+            ...s2.players[0],
+            nickel: 10,
+            penny: 10,
+            wine: 10,
+          },
+          ...s2.players.slice(1),
+        ],
+      }
+      const s4 = convert({ wine: 3 })(s3)!
+      expect(s4.players[0]).toMatchObject({
+        nickel: 10,
+        penny: 13,
+        wine: 7,
+      })
+    })
+    it('converts whiskey to coins', () => {
+      const s3 = {
+        ...s2,
+        players: [
+          {
+            ...s2.players[0],
+            nickel: 10,
+            penny: 10,
+            whiskey: 10,
+          },
+          ...s2.players.slice(1),
+        ],
+      }
+      const s4 = convert({ whiskey: 3 })(s3)!
+      expect(s4.players[0]).toMatchObject({
+        nickel: 10,
+        penny: 16,
+        whiskey: 7,
+      })
+    })
     it('converts wine if needed to uptoken to nickel', () => {
       const s3 = {
         ...s2,
