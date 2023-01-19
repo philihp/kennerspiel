fad81e609644a6777c83f5c12c4072d231d50e60
Philihp Busby <philihp@gmail.com>
2023-01-19 05:34:45 +0000

    refactor: cleanup cloisterCourtyard

--- game/src/buildings/cloisterCourtyard.ts
+++ game/src/buildings/cloisterCourtyard.ts
@@ -1,21 +1,25 @@
-import { getCost, getPlayer, payCost, setPlayer } from '../board/player'
+import { pipe } from 'ramda'
+import { getCost, payCost, withActivePlayer } from '../board/player'
 import { parseResourceParam, totalGoods, differentGoods, multiplyGoods, maskGoods } from '../board/resource'
 import { Cost, GameStatePlaying } from '../types'
 
 const ALLOWED_OUTPUT: (keyof Cost)[] = ['peat', 'clay', 'wood', 'sheep', 'grain', 'penny']
 
-export const cloisterCourtyard =
-  (input = '', output = '') =>
-  (state: GameStatePlaying | undefined): GameStatePlaying | undefined => {
+export const cloisterCourtyard = (input = '', output = '') => {
+  const inputs = parseResourceParam(input)
+  const outputs = parseResourceParam(output)
+  if (totalGoods(inputs) !== 3) return () => undefined
+  if (differentGoods(inputs) !== 3) return () => undefined
+  if (totalGoods(maskGoods(ALLOWED_OUTPUT)(outputs)) !== 1 && differentGoods(outputs) !== 1) return () => undefined
+
+  return (state: GameStatePlaying | undefined): GameStatePlaying | undefined => {
     if (state === undefined) return undefined
-    const inputs = parseResourceParam(input)
-    const outputs = parseResourceParam(output)
-    if (totalGoods(inputs) !== 3) return undefined
-    if (differentGoods(inputs) !== 3) return undefined
-    if (totalGoods(maskGoods(ALLOWED_OUTPUT)(outputs)) !== 1 && differentGoods(outputs) !== 1) return undefined
-    const player = getPlayer(state)
-    const paidCost = payCost(inputs)(player)
-    if (paidCost === undefined) return undefined
-    const gotOut = getCost(multiplyGoods(6)(outputs))(paidCost)
-    return gotOut && setPlayer(state, gotOut)
+    return withActivePlayer(
+      pipe(
+        //
+        payCost(inputs),
+        getCost(multiplyGoods(6)(outputs))
+      )
+    )(state)
   }
+}
