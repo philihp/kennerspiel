fe3a5902f6f63809e20d9bc4c7c778944f234ec3
Philihp Busby <philihp@gmail.com>
2023-01-19 22:58:54 +0000

    feature: add stubs for all buildings

---
+++ game/src/buildings/__tests__/sacristy.test.ts
@@ -0,0 +1,13 @@
+import { reducer, initialState } from '../../reducer'
+import { GameStatePlaying } from '../../types'
+
+describe('buildings/sacristy', () => {
+  describe('use', () => {
+    it('baseline happy path', () => {
+      const s0 = initialState
+      const s1 = reducer(s0, ['CONFIG', '4', 'france', 'long'])!
+      const s2 = reducer(s1, ['START', '42', 'R', 'B', 'G', 'W'])! as GameStatePlaying
+      expect(s2).toBeDefined()
+    })
+  })
+})
