049f6594872c455d79ff0930610d6dde58651a81
Philihp Busby <philihp@gmail.com>
2023-01-28 05:30:29 +0000

    refactor: market

--- game/src/buildings/__tests__/market.test.ts
+++ game/src/buildings/__tests__/market.test.ts
@@ -1,38 +1,116 @@
-import { reducer, initialState } from '../../reducer'
-import { GameStatePlaying } from '../../types'
+import { initialState } from '../../reducer'
+import {
+  GameStatePlaying,
+  GameStatusEnum,
+  NextUseClergy,
+  PlayerColor,
+  SettlementRound,
+  Tableau,
+  Tile,
+} from '../../types'
 import { market } from '../market'
 
 describe('buildings/market', () => {
   describe('market', () => {
-    it('retains undefined state', () => {
-      const s0: GameStatePlaying | undefined = undefined
-      const s1 = market()(s0)
-      expect(s1).toBeUndefined()
-    })
+    const p0: Tableau = {
+      color: PlayerColor.Blue,
+      clergy: [],
+      settlements: [],
+      landscape: [
+        [[], [], ['P'], ['P'], ['P'], ['P'], ['P'], [], []],
+        [[], [], ['P'], ['P'], ['P'], ['P'], ['P'], [], []],
+      ] as Tile[][],
+      landscapeOffset: 0,
+      peat: 0,
+      penny: 0,
+      clay: 0,
+      wood: 0,
+      grain: 0,
+      sheep: 0,
+      stone: 0,
+      flour: 0,
+      grape: 0,
+      nickel: 0,
+      hops: 0,
+      coal: 0,
+      book: 0,
+      pottery: 0,
+      whiskey: 0,
+      straw: 0,
+      meat: 0,
+      ornament: 0,
+      bread: 0,
+      wine: 0,
+      beer: 0,
+      reliquary: 0,
+    }
+    const s0: GameStatePlaying = {
+      ...initialState,
+      status: GameStatusEnum.PLAYING,
+      activePlayerIndex: 0,
+      config: {
+        country: 'france',
+        players: 3,
+        length: 'long',
+      },
+      rondel: {
+        pointingBefore: 0,
+      },
+      players: [{ ...p0 }, { ...p0 }, { ...p0 }],
+      settling: false,
+      extraRound: false,
+      moveInRound: 1,
+      round: 1,
+      startingPlayer: 1,
+      settlementRound: SettlementRound.S,
+      buildings: [],
+      nextUse: NextUseClergy.Any,
+      canBuyLandscape: true,
+      plotPurchasePrices: [1, 1, 1, 1, 1, 1],
+      districtPurchasePrices: [],
+      neutralBuildingPhase: false,
+    }
+
     it('goes through a happy path', () => {
-      const s0 = initialState
-      const s1 = reducer(s0, ['CONFIG', '1', 'france', 'short'])!
-      const s2 = reducer(s1, ['START', '42', 'R'])! as GameStatePlaying
-      s2.players[0] = {
-        ...s2.players[0],
-        stone: 2,
-        straw: 1,
-        clay: 1,
-        peat: 1,
-        penny: 1,
+      const s1 = {
+        ...s0,
+        players: [
+          {
+            ...s0.players[0],
+            straw: 1,
+            clay: 1,
+            peat: 1,
+            penny: 1,
+          },
+          ...s0.players.slice(1),
+        ],
       }
-      const s3 = reducer(s2, ['BUILD', 'F08', '3', '1'])! as GameStatePlaying
-      expect(s3.players[0].landscape[1][5]).toStrictEqual(['P', 'F08'])
-      const s4 = reducer(s3, ['USE', 'F08', 'SwClPtPn'])! as GameStatePlaying
-      expect(s4).toBeDefined()
-      expect(s4.players[0]).toMatchObject({
+      const s2 = market('SwClPtPn')(s1)!
+      expect(s2.players[0]).toMatchObject({
         straw: 0,
         clay: 0,
         peat: 0,
-        bread: 1,
-        nickel: 1,
         penny: 2,
+        nickel: 1,
+        bread: 1,
       })
+    })
+
+    it('fails if given 3 different things', () => {
+      const s1 = {
+        ...s0,
+        players: [
+          {
+            ...s0.players[0],
+            straw: 1,
+            clay: 1,
+            penny: 2,
+          },
+          ...s0.players.slice(1),
+        ],
+      }
+      const s2 = market('SwClPnPn')(s1)!
+      expect(s2).toBeUndefined()
     })
   })
 })
