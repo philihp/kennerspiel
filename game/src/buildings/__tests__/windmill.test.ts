27f073e8e314930c11224e31c1d9433c13b38461
Philihp Busby <philihp@gmail.com>
2023-01-28 05:16:30 +0000

    refactor: windmill test

--- game/src/buildings/__tests__/windmill.test.ts
+++ game/src/buildings/__tests__/windmill.test.ts
@@ -1,52 +1,99 @@
 import { reducer, initialState } from '../../reducer'
-import { BuildingEnum, GameStatePlaying } from '../../types'
+import {
+  GameStatePlaying,
+  GameStatusEnum,
+  NextUseClergy,
+  PlayerColor,
+  SettlementRound,
+  Tableau,
+  Tile,
+} from '../../types'
 import { windmill } from '../windmill'
 
-describe('buildings/cloisterCourtyard', () => {
-  describe('use', () => {
+describe('buildings/windmill', () => {
+  describe('windmill', () => {
+    const p0: Tableau = {
+      color: PlayerColor.Blue,
+      clergy: [],
+      settlements: [],
+      landscape: [
+        [[], [], ['P'], ['P'], ['P'], ['P'], ['P'], [], []],
+        [[], [], ['P'], ['P'], ['P'], ['P'], ['P'], [], []],
+      ] as Tile[][],
+      landscapeOffset: 0,
+      peat: 0,
+      penny: 100,
+      clay: 0,
+      wood: 0,
+      grain: 0,
+      sheep: 0,
+      stone: 0,
+      flour: 0,
+      grape: 0,
+      nickel: 0,
+      hops: 0,
+      coal: 0,
+      book: 0,
+      pottery: 0,
+      whiskey: 0,
+      straw: 0,
+      meat: 0,
+      ornament: 0,
+      bread: 0,
+      wine: 0,
+      beer: 0,
+      reliquary: 0,
+    }
+    const s0: GameStatePlaying = {
+      ...initialState,
+      status: GameStatusEnum.PLAYING,
+      activePlayerIndex: 0,
+      config: {
+        country: 'france',
+        players: 3,
+        length: 'long',
+      },
+      rondel: {
+        pointingBefore: 0,
+      },
+      players: [{ ...p0 }, { ...p0 }, { ...p0 }],
+      settling: false,
+      extraRound: false,
+      moveInRound: 1,
+      round: 1,
+      startingPlayer: 1,
+      settlementRound: SettlementRound.S,
+      buildings: [],
+      nextUse: NextUseClergy.Any,
+      canBuyLandscape: true,
+      plotPurchasePrices: [1, 1, 1, 1, 1, 1],
+      districtPurchasePrices: [],
+      neutralBuildingPhase: false,
+    }
+
     it('retains undefined state', () => {
       const s0: GameStatePlaying | undefined = undefined
       const s1 = windmill()(s0)
       expect(s1).toBeUndefined()
     })
     it('goes through a happy path', () => {
-      const s0 = initialState
-      const s1 = reducer(s0, ['CONFIG', '1', 'france', 'long'])!
-      const s2 = reducer(s1, ['START', '42', 'R'])! as GameStatePlaying
-      const s3 = reducer(s2, ['USE', 'LR1', 'Cl'])! as GameStatePlaying
-      expect(s3.players[0]).toMatchObject({
-        clay: 2,
-      })
-      const s4 = reducer(s3, ['COMMIT'])! as GameStatePlaying
-      const s5 = reducer(s4, ['FELL_TREES', '1', '0', 'Jo'])! as GameStatePlaying
-      expect(s5.players[0]).toMatchObject({
-        wood: 2,
-        clay: 2,
-      })
-      const s6 = reducer(s5, ['COMMIT'])! as GameStatePlaying
-      const s7 = reducer(s6, ['FELL_TREES', '1', '1'])! as GameStatePlaying
-      expect(s7.players[0]).toMatchObject({
-        wood: 5,
-        clay: 2,
-      })
-      const s8 = reducer(s7, ['COMMIT'])! as GameStatePlaying
-      const s9 = reducer(s8, ['USE', 'LR2', 'Gn'])! as GameStatePlaying
-      expect(s9.players[0]).toMatchObject({
-        grain: 3,
-      })
-      const s10 = reducer(s9, ['COMMIT'])! as GameStatePlaying
-
-      const s11 = reducer(s10, ['BUILD', 'F04', '3', '1'])! as GameStatePlaying
-      expect(s11.players[0].landscape[1][5]).toStrictEqual(['P', 'F04'])
-      expect(s11.players[0]).toMatchObject({
-        clay: 0,
-        wood: 2,
-      })
-      const s12 = reducer(s11, ['USE', 'F04', 'GnGn'])! as GameStatePlaying
-      expect(s12.players[0]).toMatchObject({
-        flour: 2,
-        straw: 2,
-        grain: 1,
+      const s1 = {
+        ...s0,
+        players: [
+          {
+            ...s0.players[0],
+            grain: 5,
+            flour: 0,
+            straw: 1,
+          },
+          ...s0.players.slice(1),
+        ],
+      }
+      const s2 = windmill('GnGnGn')(s1)! as GameStatePlaying
+      expect(s2.players[0]).toMatchObject({
+        grain: 2,
+        flour: 3,
+        straw: 4,
       })
     })
   })
