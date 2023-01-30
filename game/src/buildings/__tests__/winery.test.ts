356c92433404760dc39f4697dec21ef16e8ecdc9
Philihp Busby <philihp@gmail.com>
2023-01-30 00:38:25 +0000

    feature: add building winery

--- game/src/buildings/__tests__/winery.test.ts
+++ game/src/buildings/__tests__/winery.test.ts
@@ -1,19 +1,112 @@
-import { reducer, initialState } from '../../reducer'
-import { GameStatePlaying } from '../../types'
+import { initialState } from '../../reducer'
+import {
+  GameStatePlaying,
+  GameStatusEnum,
+  NextUseClergy,
+  PlayerColor,
+  SettlementRound,
+  Tableau,
+  Tile,
+} from '../../types'
 import { winery } from '../winery'
 
 describe('buildings/winery', () => {
-  describe('use', () => {
-    it('retains undefined state', () => {
-      const s0: GameStatePlaying | undefined = undefined
-      const s1 = winery()(s0)
-      expect(s1).toBeUndefined()
+  describe('winery', () => {
+    const p0: Tableau = {
+      color: PlayerColor.Blue,
+      clergy: [],
+      settlements: [],
+      landscape: [
+        [[], [], ['P'], ['P', 'LFO'], ['P', 'LFO'], ['P'], ['P'], [], []],
+        [[], [], ['P'], ['P', 'LFO'], ['P', 'LFO'], ['P'], ['P'], [], []],
+      ] as Tile[][],
+      landscapeOffset: 0,
+      peat: 0,
+      penny: 10,
+      clay: 0,
+      wood: 0,
+      grain: 0,
+      sheep: 10,
+      stone: 0,
+      flour: 0,
+      grape: 10,
+      nickel: 0,
+      hops: 0,
+      coal: 0,
+      book: 0,
+      pottery: 0,
+      whiskey: 0,
+      straw: 5,
+      meat: 0,
+      ornament: 0,
+      bread: 0,
+      wine: 10,
+      beer: 0,
+      reliquary: 0,
+    }
+    const s0: GameStatePlaying = {
+      ...initialState,
+      status: GameStatusEnum.PLAYING,
+      activePlayerIndex: 0,
+      config: {
+        country: 'france',
+        players: 3,
+        length: 'long',
+      },
+      rondel: {
+        pointingBefore: 0,
+      },
+      players: [{ ...p0 }, { ...p0 }, { ...p0 }],
+      settling: false,
+      extraRound: false,
+      moveInRound: 1,
+      round: 1,
+      startingPlayer: 1,
+      settlementRound: SettlementRound.S,
+      buildings: [],
+      nextUse: NextUseClergy.Any,
+      canBuyLandscape: true,
+      plotPurchasePrices: [1, 1, 1, 1, 1, 1],
+      districtPurchasePrices: [],
+      neutralBuildingPhase: false,
+    }
+
+    it('goes through a happy path', () => {
+      const s1 = winery('GpGpGp', 'Wn')(s0)! as GameStatePlaying
+      expect(s1.players[0]).toMatchObject({
+        grape: 7,
+        wine: 12,
+        nickel: 1,
+        penny: 12,
+      })
     })
-    it('baseline happy path', () => {
-      const s0 = initialState
-      const s1 = reducer(s0, ['CONFIG', '4', 'france', 'long'])!
-      const s2 = reducer(s1, ['START', '42', 'R', 'B', 'G', 'W'])! as GameStatePlaying
-      expect(s2).toBeDefined()
+
+    it('convert to wine then immediately to grape', () => {
+      const s1 = {
+        ...s0,
+        players: [{ ...s0.players[0], grape: 1, wine: 0, penny: 0, nickel: 0 }, ...s0.players.slice(1)],
+      }
+
+      const s2 = winery('Gp', 'Wn')(s1)! as GameStatePlaying
+      expect(s2.players[0]).toMatchObject({
+        grape: 0,
+        wine: 0,
+        penny: 2,
+        nickel: 1,
+      })
+    })
+
+    it('can skip grape part', () => {
+      const s1 = {
+        ...s0,
+        players: [{ ...s0.players[0], grape: 0, wine: 1, penny: 0, nickel: 0 }, ...s0.players.slice(1)],
+      }
+      const s2 = winery('', 'Wn')(s1)! as GameStatePlaying
+      expect(s2.players[0]).toMatchObject({
+        wine: 0,
+        penny: 2,
+        nickel: 1,
+      })
     })
   })
 })
