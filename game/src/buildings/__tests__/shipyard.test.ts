49a80a3238a39a8e50492af91ccce8904318ae6f
Philihp Busby <philihp@gmail.com>
2023-01-30 01:30:49 +0000

    feature: add building shipyard

--- game/src/buildings/__tests__/shipyard.test.ts
+++ game/src/buildings/__tests__/shipyard.test.ts
@@ -1,19 +1,85 @@
-import { reducer, initialState } from '../../reducer'
-import { GameStatePlaying } from '../../types'
+import { initialState } from '../../reducer'
+import {
+  GameStatePlaying,
+  GameStatusEnum,
+  NextUseClergy,
+  PlayerColor,
+  SettlementRound,
+  Tableau,
+  Tile,
+} from '../../types'
 import { shipyard } from '../shipyard'
 
 describe('buildings/shipyard', () => {
-  describe('use', () => {
-    it('retains undefined state', () => {
-      const s0: GameStatePlaying | undefined = undefined
-      const s1 = shipyard()(s0)
-      expect(s1).toBeUndefined()
-    })
-    it('baseline happy path', () => {
-      const s0 = initialState
-      const s1 = reducer(s0, ['CONFIG', '4', 'france', 'long'])!
-      const s2 = reducer(s1, ['START', '42', 'R', 'B', 'G', 'W'])! as GameStatePlaying
-      expect(s2).toBeDefined()
+  describe('shipyard', () => {
+    const p0: Tableau = {
+      color: PlayerColor.Blue,
+      clergy: [],
+      settlements: [],
+      landscape: [
+        [[], [], ['P'], ['P', 'LFO'], ['P', 'LFO'], ['P'], ['P'], [], []],
+        [[], [], ['P'], ['P', 'LFO'], ['P', 'LFO'], ['P'], ['P'], [], []],
+      ] as Tile[][],
+      landscapeOffset: 0,
+      peat: 0,
+      penny: 0,
+      clay: 0,
+      wood: 10,
+      grain: 0,
+      sheep: 0,
+      stone: 0,
+      flour: 0,
+      grape: 0,
+      nickel: 0,
+      hops: 0,
+      coal: 0,
+      book: 0,
+      pottery: 0,
+      whiskey: 0,
+      straw: 0,
+      meat: 0,
+      ornament: 0,
+      bread: 0,
+      wine: 0,
+      beer: 0,
+      reliquary: 0,
+    }
+    const s0: GameStatePlaying = {
+      ...initialState,
+      status: GameStatusEnum.PLAYING,
+      activePlayerIndex: 0,
+      config: {
+        country: 'france',
+        players: 3,
+        length: 'long',
+      },
+      rondel: {
+        pointingBefore: 3,
+        grape: 1,
+        joker: 2,
+      },
+      players: [{ ...p0 }, { ...p0 }, { ...p0 }],
+      settling: false,
+      extraRound: false,
+      moveInRound: 1,
+      round: 1,
+      startingPlayer: 1,
+      settlementRound: SettlementRound.S,
+      buildings: [],
+      nextUse: NextUseClergy.Any,
+      canBuyLandscape: true,
+      plotPurchasePrices: [1, 1, 1, 1, 1, 1],
+      districtPurchasePrices: [],
+      neutralBuildingPhase: false,
+    }
+
+    it('turns two wood into 1 ornament and 1 nickel', () => {
+      const s1 = shipyard('WoWo')(s0)! as GameStatePlaying
+      expect(s1.players[0]).toMatchObject({
+        wood: 8,
+        ornament: 1,
+        nickel: 1,
+      })
     })
   })
 })
