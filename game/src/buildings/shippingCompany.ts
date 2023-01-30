496dac2d3b5ab300da4146c71bbc9358c9d77045
Philihp Busby <philihp@gmail.com>
2023-01-30 09:20:29 +0000

    feature: add building shippingCompany

--- game/src/buildings/shippingCompany.ts
+++ game/src/buildings/shippingCompany.ts
@@ -1,15 +1,41 @@
 import { pipe } from 'ramda'
-import { GameStatePlaying } from '../types'
+import { P, match } from 'ts-pattern'
+import { getCost, payCost, withActivePlayer } from '../board/player'
+import { parseResourceParam, multiplyGoods } from '../board/resource'
+import { take } from '../board/wheel'
+import { Cost, GameStatePlaying } from '../types'
 
-const buildingStub = (state: GameStatePlaying | undefined): GameStatePlaying | undefined => {
+const takePlayerJoker = (unitCost: Cost) => (state: GameStatePlaying | undefined) => {
   if (state === undefined) return undefined
-  return state
+  const {
+    rondel: { pointingBefore, joker },
+    config,
+  } = state
+  const takeValue = take(pointingBefore, joker ?? pointingBefore, config)
+  return withActivePlayer(getCost(multiplyGoods(takeValue)(unitCost)))(state)
 }
 
-export const shippingCompany = (param1 = '', param2 = '') =>
-  pipe(
+const advanceJokerOnRondel = (state: GameStatePlaying | undefined): GameStatePlaying | undefined =>
+  state && {
+    ...state,
+    rondel: {
+      ...state.rondel,
+      joker: state.rondel.pointingBefore,
+    },
+  }
+
+export const shippingCompany = (fuel = '', product = '') => {
+  const input = parseResourceParam(fuel)
+  const output = match(parseResourceParam(product))
+    .with({ meat: P.when((n) => n > 0) }, () => ({ meat: 1 }))
+    .with({ bread: P.when((n) => n > 0) }, () => ({ bread: 1 }))
+    .with({ wine: P.when((n) => n > 0) }, () => ({ wine: 1 }))
+    .otherwise(() => ({}))
+
+  return pipe(
     //
-    buildingStub,
-    buildingStub,
-    buildingStub
+    withActivePlayer(payCost(input)),
+    takePlayerJoker(output),
+    advanceJokerOnRondel
   )
+}
