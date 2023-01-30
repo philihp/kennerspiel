3fd2e15857cb2726cea947cb8a2b1c135586d0a5
‮Philihp Busby <philihp@gmail.com>
2023-01-30 04:19:19 +0000

    feature: add building palace (#77)

--- game/src/buildings/palace.ts
+++ game/src/buildings/palace.ts
@@ -1,15 +1,43 @@
-import { pipe } from 'ramda'
-import { GameStatePlaying } from '../types'
+import { pipe, reduce } from 'ramda'
+import { payCost, withActivePlayer } from '../board/player'
+import { parseResourceParam } from '../board/resource'
+import { BuildingEnum, GameStatePlaying, NextUseClergy, Tableau, Tile } from '../types'
 
-const buildingStub = (state: GameStatePlaying | undefined): GameStatePlaying | undefined => {
-  if (state === undefined) return undefined
-  return state
-}
+const nextUseIsFree = (state: GameStatePlaying | undefined) =>
+  state && {
+    ...state,
+    nextUse: NextUseClergy.Free,
+  }
 
-export const palace = (building = '') =>
-  pipe(
-    //
-    buildingStub,
-    buildingStub,
-    buildingStub
+// given a row of tiles, return all of the buildings where there is a building AND a clergy
+const occupiedBuildingsInRow = reduce(
+  (rAccum, [_, building, clergy]: Tile) => (building && clergy ? [...rAccum, building] : rAccum),
+  [] as BuildingEnum[]
+)
+
+// given a list of rows, do the thing
+const occupiedBuildingsForLandscape = reduce(
+  (rAccum, landRow: Tile[]) => [...rAccum, ...occupiedBuildingsInRow(landRow as Tile[])],
+  [] as BuildingEnum[]
+)
+
+const occupiedBuildingsForPlayers = (players: Tableau[]) =>
+  reduce(
+    (pAccum, { landscape }) => [...pAccum, ...occupiedBuildingsForLandscape(landscape)],
+    [] as BuildingEnum[],
+    players
   )
+
+const allOccupiedBuildingsUsable = (state: GameStatePlaying | undefined): GameStatePlaying | undefined =>
+  state && { ...state, usableBuildings: occupiedBuildingsForPlayers(state.players) }
+
+export const palace = (input = '') => {
+  const { wine = 0 } = parseResourceParam(input)
+  if (wine === 0) return (state: GameStatePlaying | undefined) => state
+  return pipe(
+    //
+    withActivePlayer(payCost({ wine })),
+    nextUseIsFree,
+    allOccupiedBuildingsUsable
+  )
+}
