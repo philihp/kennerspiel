a1ffa5d94bd8df39410c8ad8332aff8da23790c1
Philihp Busby <philihp@gmail.com>
2023-02-17 10:07:28 +0000

    fix: work contract pricing and checks unoccupied

--- game/src/commands/workContract.ts
+++ game/src/commands/workContract.ts
@@ -1,6 +1,6 @@
 import { all, find, pipe, range, without } from 'ramda'
-import { findBuildingWithoutOffset, moveClergyToOwnBuilding } from '../board/landscape'
 import { payCost, getCost, withActivePlayer, isLayBrother, isPrior } from '../board/player'
+import { findBuildingWithoutOffset, moveClergyToOwnBuilding } from '../board/landscape'
 import { costMoney, parseResourceParam } from '../board/resource'
 import { oncePerFrame, revertActivePlayerToCurrent, setFrameToAllowFreeUsage, withFrame } from '../board/frame'
 import { BuildingEnum, Cost, Frame, GameCommandEnum, SettlementRound, StateReducer, Tableau } from '../types'
@@ -26,6 +26,7 @@ const transferActiveToOwnerOf =
   (building: BuildingEnum): StateReducer =>
   (state) => {
     if (state === undefined) return undefined
+    // this makes it so we dont look at the current player's landscape... prevent work contract on yourself
     const playerIndexes: number[] = without([state.frame.activePlayerIndex], range(0, state.config.players))
 
     // this makes it so we dont look at the current player's landscape... prevent work contract on yourself
