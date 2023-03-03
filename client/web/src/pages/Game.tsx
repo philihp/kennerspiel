11c5eee9c1ab070c841ddcf11f2281d49f16de46
Philihp Busby <philihp@gmail.com>
2023-03-03 13:05:00 +0000

    make Game page useful

--- client/web/src/pages/Game.tsx
+++ client/web/src/pages/Game.tsx
@@ -1,18 +1,18 @@
-import { useNavigate, useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 import { useHathoraContext } from "../context/GameContext";
 
 export default function Game() {
   const { gameId } = useParams();
-  const { disconnect, joinGame, playerState, token, user, login, connecting } =
+  const { disconnect, engineState, token, user, login, connecting } =
     useHathoraContext();
 
   useEffect(() => {
     // auto join the game once on this page
-    if (gameId && token && !playerState?.players?.find((p:any) => p.id === user?.id)) {
-      joinGame(gameId).catch(console.error);
-    }
+    // if (gameId && token && !playerState?.players?.find((p:any) => p.id === user?.id)) {
+    //   joinGame(gameId).catch(console.error);
+    // }
 
     if (!token) {
       // log the user in if they aren't already logged in
@@ -25,9 +25,14 @@ export default function Game() {
   return (
     <>
       {!connecting && token ? (
-        <div >
-          connected
-        </div>
+        <>
+          <pre>
+            USE: {JSON.stringify(user)}
+          </pre>
+          <pre>
+            STATE: {JSON.stringify(engineState, undefined, 2)}
+          </pre>
+        </>
       ) : (
         <div role="status">connecting...</div>
       )}
