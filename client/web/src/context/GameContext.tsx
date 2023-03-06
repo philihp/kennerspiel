0981b843f28d013e34f932e1867755efe1cd206b
‮Philihp Busby <philihp@gmail.com>
2023-03-06 14:49:18 +0000

    fix: linting issues with boilerplate (#151)
    * fix up linting
    
    * crazy linto

--- client/web/src/context/GameContext.tsx
+++ client/web/src/context/GameContext.tsx
@@ -1,12 +1,20 @@
-import { ToastContainer, toast } from 'react-toastify'
-import React, { createContext, ReactNode, useCallback, useContext,  useState } from 'react'
+import { ToastContainer } from 'react-toastify'
+import { createContext, ReactNode, useCallback, useContext, useMemo, useRef, useState } from 'react'
 
-import { HathoraClient } from '../../../.hathora/client'
-import { EngineState } from '../../../../api/types'
+import { HathoraClient, HathoraConnection } from '../../../.hathora/client'
+import { EngineState, IInitializeRequest } from '../../../../api/types'
+import { UserData } from '../../../../api/base'
+import { ConnectionFailure } from '../../../.hathora/failures'
 
 interface GameContext {
-    engineState?: EngineState,
-    createGame: () => Promise<string>,
+  token?: string
+  engineState?: EngineState
+  user?: UserData
+  connecting?: boolean
+  connectionError?: ConnectionFailure
+  login: () => Promise<string>
+  connect: (gameId: string) => Promise<HathoraConnection>
+  createGame: () => Promise<string>
 }
 
 interface HathoraContextProviderProps {
@@ -17,22 +25,65 @@ const client = new HathoraClient()
 const HathoraContext = createContext<GameContext | null>(null)
 
 export const HathoraContextProvider = ({ children }: HathoraContextProviderProps) => {
-  const [engineState] = useState<EngineState>()
+  const [token, setToken] = useState<string | undefined>(localStorage.getItem('token') || '')
+  const [engineState, setEngineState] = useState<EngineState>()
+  const [user, setUserInfo] = useState<UserData>()
+  const [connecting, setConnecting] = useState<boolean>()
+  const [connection, setConnection] = useState<HathoraConnection>()
+  const [connectionError, setConnectionError] = useState<ConnectionFailure>()
 
-  const createGame = useCallback(async () => new Promise<string>((fulfill, _reject) => {
-    setTimeout(() => {
-      fulfill("ok")
-    }, 500)
-  }),
-  [])
+  const login = useCallback(async (): Promise<string> => {
+    const token = await client.loginAnonymous()
+    localStorage.setItem('token', token)
+    setToken(token)
+    const user = HathoraClient.getUserFromToken(token)
+    setUserInfo(user)
+    return token
+  }, [])
+
+  const createGame = useCallback(async (): Promise<string> => {
+    let currentToken = token
+    if (currentToken) {
+      return client.create(currentToken, IInitializeRequest.default())
+    }
+    currentToken = await login()
+    return client.create(currentToken, IInitializeRequest.default())
+  }, [token, login])
+
+  const connect = useCallback(
+    async (stateId: string) => {
+      setConnecting(true)
+      let currentToken = token
+      if (!currentToken) {
+        currentToken = await login()
+      }
+      const connection = await client.connect(
+        currentToken,
+        stateId,
+        ({ state }) => setEngineState(state),
+        setConnectionError
+      )
+      setConnection(connection)
+      setConnecting(false)
+      return connection
+    },
+    [token, login]
+  )
+
+  const exported = useMemo(
+    () => ({
+      token,
+      engineState,
+      user,
+      login,
+      connect,
+      createGame,
+    }),
+    [token, engineState, user, login, connect, createGame]
+  )
 
   return (
-    <HathoraContext.Provider
-      value={{
-        engineState,
-        createGame,
-      }}
-    >
+    <HathoraContext.Provider value={exported}>
       {children}
       <ToastContainer
         autoClose={1000}
