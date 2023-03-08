bb2de1668dfec5ed3e25921a3bed0b9484134a91
Philihp Busby <philihp@gmail.com>
2023-03-08 08:20:27 +0000

    show player resources and settlements and wonders

---
+++ client/web/src/components/PlayerWonders.tsx
@@ -0,0 +1,14 @@
+import { range } from 'ramda'
+
+interface Props {
+  wonders: number
+}
+
+export const PlayerWonders = ({ wonders }: Props) => (
+  <div style={{ margin: 10 }}>
+    Wonders:
+    {range(0, wonders).map((n) => (
+      <span key={n}>🖼️</span>
+    ))}
+  </div>
+)
