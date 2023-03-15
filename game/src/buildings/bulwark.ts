9c195ca7bdaca29f14f26e7206ad4588974206b7
Philihp Busby <philihp@gmail.com>
2023-03-15 10:30:01 +0000

    wip

--- game/src/buildings/bulwark.ts
+++ game/src/buildings/bulwark.ts
@@ -2,7 +2,7 @@ import { identity, pipe } from 'ramda'
 import { parseResourceParam } from '../board/resource'
 import { withActivePlayer, payCost } from '../board/player'
 import { GameCommandEnum } from '../types'
-import { addBonusAction } from '../board/frame'
+import { addBonusAction, withFrame } from '../board/frame'
 
 export const bulwark = (coin = '') => {
   const { book = 0 } = parseResourceParam(coin)
@@ -10,6 +10,7 @@ export const bulwark = (coin = '') => {
   return pipe(
     //
     withActivePlayer(payCost({ book })),
+    addLandscapeGrants(2),
     addBonusAction(GameCommandEnum.BUY_DISTRICT),
     addBonusAction(GameCommandEnum.BUY_PLOT)
   )
