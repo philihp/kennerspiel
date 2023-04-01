c8da6870fe718f093fe72069d7c8dcd4227a1a01
Philihp Busby <philihp@gmail.com>
2023-04-01 07:34:28 +0000

    revert canGrantLandscape method

--- game/src/buildings/bulwark.ts
+++ game/src/buildings/bulwark.ts
@@ -2,7 +2,7 @@ import { identity, pipe } from 'ramda'
 import { parseResourceParam } from '../board/resource'
 import { withActivePlayer, payCost } from '../board/player'
 import { GameCommandEnum } from '../types'
-import { addBonusAction, withFrame } from '../board/frame'
+import { addBonusAction } from '../board/frame'
 
 export const bulwark = (coin = '') => {
   const { book = 0 } = parseResourceParam(coin)
@@ -10,7 +10,6 @@ export const bulwark = (coin = '') => {
   return pipe(
     //
     withActivePlayer(payCost({ book })),
-    addLandscapeGrants(2),
     addBonusAction(GameCommandEnum.BUY_DISTRICT),
     addBonusAction(GameCommandEnum.BUY_PLOT)
   )
