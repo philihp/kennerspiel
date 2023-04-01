8d48dbcc187710677de72440716ceeb9d7832812
Philihp Busby <philihp@gmail.com>
2023-04-01 06:43:38 +0000

    fix: resolve todo on 2 clergy, but add todo for additional prod

--- game/src/board/frame/nextFrame3Short.ts
+++ game/src/board/frame/nextFrame3Short.ts
@@ -10,7 +10,7 @@ import { introduceSettlements } from '../settlements'
 import { removeHomelandForestMoor } from './removeHomelandForestMoor'
 import { standardResources } from './standardResources'
 
-// TODO: only 2 clergymen
+// TODO: 1 good goes to everyone when anyone uses the production wheel
 
 export const nextFrame3Short: FrameFlow = {
   // Round 1
