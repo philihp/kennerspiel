2503c82b840080130d525ab0ed86697fb6840543
Philihp Busby <philihp@gmail.com>
2023-04-16 06:57:19 +0000

    feature: add completions for SETTLE

--- game/src/commands/__tests__/settle.test.ts
+++ game/src/commands/__tests__/settle.test.ts
@@ -4,7 +4,6 @@ import {
   GameCommandEnum,
   GameStatePlaying,
   GameStatusEnum,
-  LandEnum,
   NextUseClergy,
   PlayerColor,
   SettlementEnum,
@@ -143,9 +142,179 @@ describe('commands/build', () => {
   })
 
   describe('complete', () => {
-    it('stub', () => {
-      const c0 = complete(s0, [])
+    it('allows running if in a settlement frame', () => {
+      const s1: GameStatePlaying = {
+        ...s0,
+        frame: {
+          ...s0.frame,
+          bonusActions: [GameCommandEnum.SETTLE],
+        },
+      }
+      const c0 = complete(s1, [])
+      expect(c0).toStrictEqual(['SETTLE'])
+    })
+    it('does not allow running if not in a settlement frame', () => {
+      const s1: GameStatePlaying = {
+        ...s0,
+        frame: {
+          ...s0.frame,
+          bonusActions: [],
+        },
+      }
+      const c0 = complete(s1, [])
       expect(c0).toStrictEqual([])
+    })
+    it('does not allow if none of their settlements can be built', () => {
+      const s1: GameStatePlaying = {
+        ...s0,
+        frame: {
+          ...s0.frame,
+          bonusActions: [],
+        },
+      }
+      const c0 = complete(s1, [])
+      expect(c0).toStrictEqual([])
+    })
+    it('gives all of the active player settlements, if there are any', () => {
+      const s1: GameStatePlaying = {
+        ...s0,
+        players: [
+          {
+            ...s0.players[0],
+            settlements: ['SR1', 'SR3', 'SR5', 'SR6'] as SettlementEnum[],
+          },
+          ...s0.players.slice(1),
+        ],
+      }
+      const c0 = complete(s1, ['SETTLE'])
+      expect(c0).toStrictEqual(['SR1', 'SR3', 'SR5', 'SR6'])
+    })
+    it('only gives settlements that the player can afford', () => {
+      const s1: GameStatePlaying = {
+        ...s0,
+        players: [
+          {
+            ...s0.players[0],
+            settlements: ['SR1', 'SR2', 'SR3', 'SR4'] as SettlementEnum[],
+            wood: 1,
+            peat: 0,
+            coal: 0,
+            straw: 0,
+          },
+          ...s0.players.slice(1),
+        ],
+      }
+      const c0 = complete(s1, ['SETTLE'])
+      expect(c0).toStrictEqual(['SR1', 'SR3'])
+    })
+    it('only gives spots that match the building', () => {
+      const s1: GameStatePlaying = {
+        ...s0,
+        players: [
+          {
+            ...s0.players[0],
+            landscape: [
+              [['W'], ['C'], ['P', 'LPE'], ['P', 'LFO'], ['P', 'LFO'], ['H'], ['H'], [], []],
+              [['W'], ['C'], ['P', 'LPE'], ['P', 'LFO'], ['P', 'LFO'], ['H'], ['P', 'LB1'], [], []],
+              [[], [], ['P', 'LPE'], ['P', 'LFO'], ['P', 'LB2'], ['P'], ['P', 'LB3'], [], []],
+            ] as Tile[][],
+            landscapeOffset: 1,
+          },
+          ...s0.players.slice(1),
+        ],
+      }
+      const c0 = complete(s1, ['SETTLE', 'SR4']) // Fishing Village can only be on coast
+      expect(c0).toStrictEqual(['-1 -1', '-1 0'])
+    })
+    it('gives no settlements if there are none', () => {
+      const s1: GameStatePlaying = {
+        ...s0,
+        players: [
+          {
+            ...s0.players[0],
+            landscape: [
+              [['W'], ['C'], ['P', 'LPE'], ['P', 'LFO'], ['P', 'LFO'], ['H'], ['H'], [], []],
+              [['W'], ['C'], ['P', 'LPE'], ['P', 'LFO'], ['P', 'LFO'], ['H'], ['P', 'LB1'], [], []],
+              [[], [], ['P', 'LPE'], ['P', 'LFO'], ['P', 'LB2'], ['P'], ['P', 'LB3'], [], []],
+            ] as Tile[][],
+            landscapeOffset: 1,
+          },
+          ...s0.players.slice(1),
+        ],
+      }
+      const c0 = complete(s1, ['SETTLE', 'SR6'])
+      expect(c0).toStrictEqual(['-1 -1', '3 -1', '4 -1', '-1 0', '3 0', '3 1'])
+    })
+    it('gives rows that the first col is free', () => {
+      const s1: GameStatePlaying = {
+        ...s0,
+        players: [
+          {
+            ...s0.players[0],
+            landscape: [
+              [['W'], ['C'], ['P', 'LPE'], ['P', 'LFO'], ['P', 'LFO'], ['H'], ['H'], [], []],
+              [['W'], ['C'], ['P', 'LPE'], ['P', 'LFO'], ['P', 'LFO'], ['H'], ['P', 'LB1'], [], []],
+              [[], [], ['P', 'LPE'], ['P', 'LFO'], ['P', 'LB2'], ['P'], ['P', 'LB3'], [], []],
+            ] as Tile[][],
+            landscapeOffset: 1,
+          },
+          ...s0.players.slice(1),
+        ],
+      }
+      const c0 = complete(s1, ['SETTLE', 'SR8', '3'])
+      expect(c0).toStrictEqual(['-1', '0'])
+    })
+    it('gives ways of paying for the given thing', () => {
+      const s1: GameStatePlaying = {
+        ...s0,
+        players: [
+          {
+            ...s0.players[0],
+            landscape: [
+              [['W'], ['C'], ['P', 'LPE'], ['P', 'LFO'], ['P', 'LFO'], ['H'], ['H'], [], []],
+              [['W'], ['C'], ['P', 'LPE'], ['P', 'LFO'], ['P', 'LFO'], ['H'], ['P', 'LB1'], [], []],
+              [[], [], ['P', 'LPE'], ['P', 'LFO'], ['P', 'LB2'], ['P'], ['P', 'LB3'], [], []],
+            ] as Tile[][],
+            landscapeOffset: 1,
+            peat: 0,
+            penny: 1,
+            clay: 0,
+            wood: 5,
+            grain: 1,
+            sheep: 5,
+            stone: 0,
+            flour: 0,
+            grape: 0,
+            nickel: 0,
+            malt: 0,
+            coal: 5,
+            book: 0,
+            ceramic: 0,
+            whiskey: 0,
+            straw: 0,
+            meat: 5,
+            ornament: 0,
+            bread: 0,
+            wine: 0,
+            beer: 0,
+            reliquary: 0,
+          },
+          ...s0.players.slice(1),
+        ],
+      }
+      const c0 = complete(s1, ['SETTLE', 'SR6', '-1', ' 0'])
+      // 5 food 6 energy
+      expect(c0).toStrictEqual([
+        // this sort is intentional, because people probably want to eat meat first
+        'MtCoCo',
+        'MtCoWoWoWo',
+        'ShShShCoCo',
+        'ShShShCoWoWoWo',
+        'ShShGnCoCo',
+        'ShShGnCoWoWoWo',
+        'ShShPnCoCo',
+        'ShShPnCoWoWoWo',
+      ])
     })
   })
 })
--- game/src/commands/settle.ts
+++ game/src/commands/settle.ts
@@ -1,9 +1,10 @@
-import { curry, filter, pipe } from 'ramda'
-import { addErectionAtLandscape } from '../board/erections'
+import { addIndex, curry, filter, map, pipe, reduce, view } from 'ramda'
+import { P, match } from 'ts-pattern'
+import { addErectionAtLandscape, terrainForErection } from '../board/erections'
 import { onlyViaBonusActions } from '../board/frame'
 import { checkLandscapeFree, checkLandTypeMatches } from '../board/landscape'
-import { payCost, withActivePlayer } from '../board/player'
-import { costEnergy, costFood, parseResourceParam } from '../board/resource'
+import { activeLens, payCost, withActivePlayer } from '../board/player'
+import { costEnergy, costFood, parseResourceParam, settlementCostOptions } from '../board/resource'
 import { costForSettlement } from '../board/settlements'
 import {
   Cost,
@@ -12,6 +13,8 @@ import {
   GameStatePlaying,
   SettlementEnum,
   StateReducer,
+  Tableau,
+  Tile,
 } from '../types'
 
 const payForSettlement = (settlement: SettlementEnum, resources: Cost) =>
@@ -37,6 +40,37 @@ const removeSettlementFromUnbuilt = (settlement: SettlementEnum): StateReducer =
     }
   })
 
+const settleableLocationsCol = (settlement: SettlementEnum, rawCol: string, player: Tableau): string[] => {
+  const col = Number.parseInt(rawCol, 10) + 2
+  const colsAtRow = map((row: Tile[]) => row[col], player.landscape)
+  return addIndex(reduce<Tile, string[]>)(
+    (accum, tile, rowIndex) => {
+      if (tile[0] && terrainForErection(settlement).includes(tile[0]) && !tile[1])
+        accum.push(`${rowIndex - player.landscapeOffset}`)
+      return accum
+    },
+    [] as string[],
+    colsAtRow
+  )
+}
+
+const settleableLocations = curry((settlement: SettlementEnum, player: Tableau): string[] =>
+  addIndex(reduce<Tile[], string[]>)(
+    (accum, row, rowIndex) =>
+      addIndex(reduce<Tile, string[]>)(
+        (innerAccum, tile, colIndex) => {
+          if (tile[0] && terrainForErection(settlement).includes(tile[0]) && !tile[1])
+            innerAccum.push(`${colIndex - 2} ${rowIndex - player.landscapeOffset}`)
+          return innerAccum
+        },
+        accum,
+        row
+      ),
+    [] as string[],
+    player.landscape
+  )
+)
+
 export const settle = ({ row, col, settlement, resources }: GameCommandSettleParams) => {
   const input = parseResourceParam(resources)
   return pipe(
@@ -51,5 +85,43 @@ export const settle = ({ row, col, settlement, resources }: GameCommandSettlePar
 }
 
 export const complete = curry((state: GameStatePlaying, partial: string[]): string[] => {
-  return []
+  const player = view(activeLens(state), state)
+  return match<string[], string[]>(partial)
+    .with([], () => {
+      if (
+        pipe(
+          //
+          onlyViaBonusActions(GameCommandEnum.SETTLE)
+          // would be cool to check, recursively, if any of the possibilities to follow
+          // this were also allowable, i.e. they complete into an array that is non-zero in length
+          //
+          // by not doing this, it's possible to go down a dead end path, and that might not be fun
+          // for the user. but also that's really hard. so maybe not right now... i'll leave this as a
+          // TODO
+        )(state) !== undefined
+      )
+        return [GameCommandEnum.SETTLE]
+      return []
+    })
+    .with([GameCommandEnum.SETTLE], () => {
+      return filter((settlement) => {
+        const playerFood = costFood(player)
+        const playerEnergy = costEnergy(player)
+        const { food: requiredFood, energy: requiredEnergy } = costForSettlement(settlement)
+        return playerFood >= requiredFood && playerEnergy >= requiredEnergy
+      }, view(activeLens(state), state).settlements)
+    })
+    .with([GameCommandEnum.SETTLE, P._], ([, settlement]) => settleableLocations(settlement as SettlementEnum, player))
+    .with([GameCommandEnum.SETTLE, P._, P._], ([, settlement, col]) =>
+      settleableLocationsCol(settlement as SettlementEnum, col, player)
+    )
+    .with([GameCommandEnum.SETTLE, P._, P._, P._], ([, settlement, _col, _row]) => {
+      return settlementCostOptions(costForSettlement(settlement as SettlementEnum), player)
+    })
+    .with([GameCommandEnum.SETTLE, P._, P._, P._, P._], ([, settlement, col, row, resources]) => {
+      const r = Number.parseInt(row, 10)
+      const c = Number.parseInt(col, 10) - 2
+      return ['']
+    })
+    .otherwise(() => [])
 })
