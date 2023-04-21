0aa91f9c8c892c08ed5a2ece8e8a1a87b80d0e12
Philihp Busby <philihp@gmail.com>
2023-04-21 01:26:14 +0000

    feature: add completions for USE cottage

--- game/src/buildings/cottage.ts
+++ game/src/buildings/cottage.ts
@@ -1,7 +1,8 @@
-import { pipe } from 'ramda'
+import { always, curry, pipe } from 'ramda'
+import { match } from 'ts-pattern'
 import { allowFreeUsageToNeighborsOf, disableFurtherUsage } from '../board/frame'
 import { getCost, withActivePlayer } from '../board/player'
-import { BuildingEnum, StateReducer } from '../types'
+import { BuildingEnum, GameStatePlaying, StateReducer } from '../types'
 
 export const cottage = (): StateReducer =>
   pipe(
@@ -9,3 +10,9 @@ export const cottage = (): StateReducer =>
     disableFurtherUsage(BuildingEnum.Cottage),
     allowFreeUsageToNeighborsOf(BuildingEnum.Cottage)
   )
+
+export const complete = curry((partial: string[], _state: GameStatePlaying): string[] =>
+  match(partial)
+    .with([], always(['']))
+    .otherwise(always([]))
+)
