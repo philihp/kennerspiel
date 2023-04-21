5b9ac7d5ea2baad69c0e761d6ced862e12c2f0b6
Philihp Busby <philihp@gmail.com>
2023-04-21 01:15:13 +0000

    feature: add completions for USE peatCoalKiln

--- game/src/buildings/peatCoalKiln.ts
+++ game/src/buildings/peatCoalKiln.ts
@@ -1,6 +1,8 @@
-import { pipe } from 'ramda'
-import { getCost, payCost, withActivePlayer } from '../board/player'
-import { parseResourceParam } from '../board/resource'
+import { always, curry, map, pipe, range, reverse, view } from 'ramda'
+import { P, match } from 'ts-pattern'
+import { activeLens, getCost, payCost, withActivePlayer } from '../board/player'
+import { parseResourceParam, stringRepeater } from '../board/resource'
+import { GameStatePlaying } from '../types'
 
 export const peatCoalKiln = (param = '') => {
   const { peat = 0 } = parseResourceParam(param)
@@ -12,3 +14,13 @@ export const peatCoalKiln = (param = '') => {
     )
   )
 }
+
+export const complete = curry((partial: string[], state: GameStatePlaying): string[] =>
+  match(partial)
+    .with([], () => {
+      const { peat = 0 } = view(activeLens(state), state)
+      return map<number, string>(stringRepeater('Pt'), reverse(range(0, 1 + peat)))
+    })
+    .with([P._], always(['']))
+    .otherwise(always([]))
+)
