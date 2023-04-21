8b96d0411f53feb85235893e4d474fc0584dd8a7
Philihp Busby <philihp@gmail.com>
2023-04-21 01:24:49 +0000

    feature: add completions for USE cloisterGarden

--- game/src/buildings/__tests__/cloisterGarden.test.ts
+++ game/src/buildings/__tests__/cloisterGarden.test.ts
@@ -8,77 +8,76 @@ import {
   Tile,
   GameStatePlaying,
 } from '../../types'
-import { cloisterGarden } from '..'
+import { cloisterGarden, complete } from '../cloisterGarden'
 
 describe('buildings/cloisterGarden', () => {
+  const p0: Tableau = {
+    color: PlayerColor.Blue,
+    clergy: [],
+    settlements: [],
+    landscape: [
+      [[], [], ['P'], ['P'], ['P'], ['P'], ['P'], [], []],
+      [[], [], ['P'], ['P'], ['P'], ['P'], ['P'], [], []],
+    ] as Tile[][],
+    wonders: 0,
+    landscapeOffset: 0,
+    peat: 0,
+    penny: 0,
+    clay: 0,
+    wood: 0,
+    grain: 0,
+    sheep: 0,
+    stone: 0,
+    flour: 0,
+    grape: 0,
+    nickel: 0,
+    malt: 0,
+    coal: 0,
+    book: 0,
+    ceramic: 0,
+    whiskey: 0,
+    straw: 0,
+    meat: 0,
+    ornament: 0,
+    bread: 0,
+    wine: 0,
+    beer: 0,
+    reliquary: 0,
+  }
+  const s0: GameStatePlaying = {
+    ...initialState,
+    status: GameStatusEnum.PLAYING,
+    config: {
+      country: 'france',
+      players: 3,
+      length: 'long',
+    },
+    rondel: {
+      pointingBefore: 0,
+    },
+    wonders: 0,
+    players: [{ ...p0 }, { ...p0 }, { ...p0 }],
+    buildings: [],
+    plotPurchasePrices: [1, 1, 1, 1, 1, 1],
+    districtPurchasePrices: [],
+    frame: {
+      round: 1,
+      next: 1,
+      startingPlayer: 1,
+      settlementRound: SettlementRound.S,
+      currentPlayerIndex: 0,
+      activePlayerIndex: 0,
+      neutralBuildingPhase: false,
+      bonusRoundPlacement: false,
+      mainActionUsed: false,
+      bonusActions: [],
+      canBuyLandscape: true,
+      unusableBuildings: [],
+      usableBuildings: [],
+      nextUse: NextUseClergy.Any,
+    },
+  }
   describe('cloisterGarden', () => {
-    const p0: Tableau = {
-      color: PlayerColor.Blue,
-      clergy: [],
-      settlements: [],
-      landscape: [
-        [[], [], ['P'], ['P'], ['P'], ['P'], ['P'], [], []],
-        [[], [], ['P'], ['P'], ['P'], ['P'], ['P'], [], []],
-      ] as Tile[][],
-      wonders: 0,
-      landscapeOffset: 0,
-      peat: 0,
-      penny: 0,
-      clay: 0,
-      wood: 0,
-      grain: 0,
-      sheep: 0,
-      stone: 0,
-      flour: 0,
-      grape: 0,
-      nickel: 0,
-      malt: 0,
-      coal: 0,
-      book: 0,
-      ceramic: 0,
-      whiskey: 0,
-      straw: 0,
-      meat: 0,
-      ornament: 0,
-      bread: 0,
-      wine: 0,
-      beer: 0,
-      reliquary: 0,
-    }
-    const s0: GameStatePlaying = {
-      ...initialState,
-      status: GameStatusEnum.PLAYING,
-      config: {
-        country: 'france',
-        players: 3,
-        length: 'long',
-      },
-      rondel: {
-        pointingBefore: 0,
-      },
-      wonders: 0,
-      players: [{ ...p0 }, { ...p0 }, { ...p0 }],
-      buildings: [],
-      plotPurchasePrices: [1, 1, 1, 1, 1, 1],
-      districtPurchasePrices: [],
-      frame: {
-        round: 1,
-        next: 1,
-        startingPlayer: 1,
-        settlementRound: SettlementRound.S,
-        currentPlayerIndex: 0,
-        activePlayerIndex: 0,
-        neutralBuildingPhase: false,
-        bonusRoundPlacement: false,
-        mainActionUsed: false,
-        bonusActions: [],
-        canBuyLandscape: true,
-        unusableBuildings: [],
-        usableBuildings: [],
-        nextUse: NextUseClergy.Any,
-      },
-    }
-
     it('retains undefined state', () => {
       const s1 = cloisterGarden()(undefined)!
       expect(s1).toBeUndefined()
@@ -136,6 +135,17 @@ describe('buildings/cloisterGarden', () => {
         usableBuildings: [],
       })
       expect(s2.players[0].grape).toBe(1)
+    })
+  })
+
+  describe('complete', () => {
+    it('takes no parameters', () => {
+      const c0 = complete([])(s0)
+      expect(c0).toStrictEqual([''])
+    })
+    it('does not complete anything with a param', () => {
+      const c0 = complete([''])(s0)
+      expect(c0).toStrictEqual([])
     })
   })
 })
--- game/src/buildings/cloisterGarden.ts
+++ game/src/buildings/cloisterGarden.ts
@@ -1,7 +1,8 @@
-import { pipe } from 'ramda'
+import { always, curry, pipe } from 'ramda'
+import { match } from 'ts-pattern'
 import { allowFreeUsageToNeighborsOf, disableFurtherUsage } from '../board/frame'
 import { getCost, withActivePlayer } from '../board/player'
-import { BuildingEnum, StateReducer } from '../types'
+import { BuildingEnum, GameStatePlaying, StateReducer } from '../types'
 
 export const cloisterGarden = (): StateReducer =>
   pipe(
@@ -9,3 +10,9 @@ export const cloisterGarden = (): StateReducer =>
     disableFurtherUsage(BuildingEnum.CloisterGarden),
     allowFreeUsageToNeighborsOf(BuildingEnum.CloisterGarden)
   )
+
+export const complete = curry((partial: string[], _state: GameStatePlaying): string[] =>
+  match(partial)
+    .with([], always(['']))
+    .otherwise(always([]))
+)
