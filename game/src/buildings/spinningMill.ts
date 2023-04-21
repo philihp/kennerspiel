bd03a12b737b500abea288ac04c33af17280471f
Philihp Busby <philihp@gmail.com>
2023-04-21 01:46:38 +0000

    feature: add completions for USE spinningMill

--- game/src/buildings/spinningMill.ts
+++ game/src/buildings/spinningMill.ts
@@ -1,12 +1,24 @@
+import { T, __, always, cond, curry, gte } from 'ramda'
+import { match } from 'ts-pattern'
 import { getCost, withActivePlayer } from '../board/player'
-import { StateReducer, Tableau } from '../types'
-
-const coinsForSheep = (sheep: number) => (sheep >= 9 ? 1 : 0) + (sheep >= 5 ? 2 : 0) + (sheep >= 1 ? 3 : 0)
+import { GameStatePlaying, StateReducer } from '../types'
 
 export const spinningMill = (): StateReducer => {
   return withActivePlayer((player) => {
+    // TODO: this could be an evolve?
     return getCost({
-      penny: coinsForSheep(player?.sheep ?? 0),
+      penny: cond([
+        [gte(__, 9), always(6)],
+        [gte(__, 5), always(5)],
+        [gte(__, 1), always(3)],
+        [T, always(0)],
+      ])(player.sheep ?? 0),
     })(player)
   })
 }
+
+export const complete = curry((partial: string[], _state: GameStatePlaying): string[] =>
+  match(partial)
+    .with([], always(['']))
+    .otherwise(always([]))
+)
