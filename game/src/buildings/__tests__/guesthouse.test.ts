42cd6939be2cba389f02311dfd15e6e75427c4ef
Philihp Busby <philihp@gmail.com>
2023-04-21 01:28:34 +0000

    feature: add completions for USE guesthouse

--- game/src/buildings/__tests__/guesthouse.test.ts
+++ game/src/buildings/__tests__/guesthouse.test.ts
@@ -9,79 +9,78 @@ import {
   Tableau,
   Tile,
 } from '../../types'
-import { guesthouse } from '../guesthouse'
+import { guesthouse, complete } from '../guesthouse'
 
 describe('buildings/guesthouse', () => {
+  const p0: Tableau = {
+    color: PlayerColor.Blue,
+    clergy: [],
+    settlements: [],
+    landscape: [
+      [[], [], ['P'], ['P', 'LFO'], ['P', 'LFO'], ['P'], ['H', 'LB1', 'LB1B'], [], []],
+      [[], [], ['P'], ['P', 'LFO'], ['P', 'LB2'], ['P'], ['P', 'LB3'], [], []],
+    ] as Tile[][],
+    wonders: 0,
+    landscapeOffset: 0,
+    peat: 0,
+    penny: 0,
+    clay: 0,
+    wood: 0,
+    grain: 0,
+    sheep: 0,
+    stone: 0,
+    flour: 0,
+    grape: 0,
+    nickel: 0,
+    malt: 0,
+    coal: 0,
+    book: 0,
+    ceramic: 0,
+    whiskey: 0,
+    straw: 0,
+    meat: 0,
+    ornament: 0,
+    bread: 0,
+    wine: 0,
+    beer: 0,
+    reliquary: 0,
+  }
+  const s0: GameStatePlaying = {
+    ...initialState,
+    status: GameStatusEnum.PLAYING,
+    frame: {
+      round: 1,
+      next: 1,
+      startingPlayer: 1,
+      settlementRound: SettlementRound.S,
+      currentPlayerIndex: 0,
+      activePlayerIndex: 0,
+      neutralBuildingPhase: false,
+      bonusRoundPlacement: false,
+      mainActionUsed: false,
+      bonusActions: [],
+      canBuyLandscape: true,
+      unusableBuildings: [],
+      usableBuildings: [],
+      nextUse: NextUseClergy.Any,
+    },
+    config: {
+      country: 'france',
+      players: 3,
+      length: 'long',
+    },
+    rondel: {
+      pointingBefore: 3,
+      grape: 1,
+      joker: 2,
+    },
+    wonders: 0,
+    players: [{ ...p0 }, { ...p0 }, { ...p0 }],
+    buildings: ['I27', 'G28', 'I29', 'I30'] as BuildingEnum[],
+    plotPurchasePrices: [1, 1, 1, 1, 1, 1],
+    districtPurchasePrices: [],
+  }
   describe('guesthouse', () => {
-    const p0: Tableau = {
-      color: PlayerColor.Blue,
-      clergy: [],
-      settlements: [],
-      landscape: [
-        [[], [], ['P'], ['P', 'LFO'], ['P', 'LFO'], ['P'], ['H', 'LB1', 'LB1B'], [], []],
-        [[], [], ['P'], ['P', 'LFO'], ['P', 'LB2'], ['P'], ['P', 'LB3'], [], []],
-      ] as Tile[][],
-      wonders: 0,
-      landscapeOffset: 0,
-      peat: 0,
-      penny: 0,
-      clay: 0,
-      wood: 0,
-      grain: 0,
-      sheep: 0,
-      stone: 0,
-      flour: 0,
-      grape: 0,
-      nickel: 0,
-      malt: 0,
-      coal: 0,
-      book: 0,
-      ceramic: 0,
-      whiskey: 0,
-      straw: 0,
-      meat: 0,
-      ornament: 0,
-      bread: 0,
-      wine: 0,
-      beer: 0,
-      reliquary: 0,
-    }
-    const s0: GameStatePlaying = {
-      ...initialState,
-      status: GameStatusEnum.PLAYING,
-      frame: {
-        round: 1,
-        next: 1,
-        startingPlayer: 1,
-        settlementRound: SettlementRound.S,
-        currentPlayerIndex: 0,
-        activePlayerIndex: 0,
-        neutralBuildingPhase: false,
-        bonusRoundPlacement: false,
-        mainActionUsed: false,
-        bonusActions: [],
-        canBuyLandscape: true,
-        unusableBuildings: [],
-        usableBuildings: [],
-        nextUse: NextUseClergy.Any,
-      },
-      config: {
-        country: 'france',
-        players: 3,
-        length: 'long',
-      },
-      rondel: {
-        pointingBefore: 3,
-        grape: 1,
-        joker: 2,
-      },
-      wonders: 0,
-      players: [{ ...p0 }, { ...p0 }, { ...p0 }],
-      buildings: ['I27', 'G28', 'I29', 'I30'] as BuildingEnum[],
-      plotPurchasePrices: [1, 1, 1, 1, 1, 1],
-      districtPurchasePrices: [],
-    }
-
     it('retains undefined state', () => {
       const s1 = guesthouse()(undefined)!
       expect(s1).toBeUndefined()
@@ -95,6 +94,17 @@ describe('buildings/guesthouse', () => {
     it('adds usable buildings', () => {
       const s1 = guesthouse()(s0)!
       expect(s1.frame.usableBuildings.sort()).toStrictEqual(['I27', 'G28', 'I29', 'I30'].sort())
+    })
+  })
+
+  describe('complete', () => {
+    it('takes no parameters', () => {
+      const c0 = complete([])(s0)
+      expect(c0).toStrictEqual([''])
+    })
+    it('does not complete anything with a param', () => {
+      const c0 = complete([''])(s0)
+      expect(c0).toStrictEqual([])
     })
   })
 })
