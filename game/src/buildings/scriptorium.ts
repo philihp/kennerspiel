7119e0f933b2c1c01fc143cddae9fd277bfd0d66
Philihp Busby <philihp@gmail.com>
2023-04-22 03:56:05 +0000

    feature: add completions for USE scriptorium

--- game/src/buildings/scriptorium.ts
+++ game/src/buildings/scriptorium.ts
@@ -1,6 +1,8 @@
-import { pipe } from 'ramda'
-import { getCost, payCost, withActivePlayer } from '../board/player'
+import { always, curry, evolve, pipe, view } from 'ramda'
+import { P, match } from 'ts-pattern'
+import { activeLens, getCost, payCost, withActivePlayer } from '../board/player'
 import { costMoney, parseResourceParam } from '../board/resource'
+import { GameStatePlaying } from '../types'
 
 export const scriptorium = (pennies = '') => {
   const input = parseResourceParam(pennies)
@@ -16,3 +18,24 @@ export const scriptorium = (pennies = '') => {
     )
   )
 }
+
+export const complete = curry((partial: string[], state: GameStatePlaying): string[] =>
+  match(partial)
+    .with([], () => {
+      const outputs: string[] = []
+      const attachIfTruthy = (token: string) => (n?: number) => n && outputs.push(token)
+      // okay i guess
+      evolve(
+        {
+          penny: attachIfTruthy('Pn'),
+          nickel: attachIfTruthy('Ni'),
+          wine: attachIfTruthy('Wn'),
+          whiskey: attachIfTruthy('Wh'),
+        },
+        view(activeLens(state), state)
+      )
+      return [...outputs, '']
+    })
+    .with([P._], always(['']))
+    .otherwise(always([]))
+)
