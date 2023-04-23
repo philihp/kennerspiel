558e9374d3ce9f0b66ef4815fe3f1c5da476461c
Philihp Busby <philihp@gmail.com>
2023-04-23 06:46:42 +0000

    feature: add completions for USE chapel

--- game/src/buildings/chapel.ts
+++ game/src/buildings/chapel.ts
@@ -1,6 +1,8 @@
-import { pipe } from 'ramda'
-import { getCost, payCost, withActivePlayer } from '../board/player'
-import { parseResourceParam } from '../board/resource'
+import { always, concat, curry, lift, min, pipe, unnest, view, zip, zipWith } from 'ramda'
+import { P, match } from 'ts-pattern'
+import { activeLens, getCost, payCost, withActivePlayer } from '../board/player'
+import { parseResourceParam, resourceArray } from '../board/resource'
+import { GameStatePlaying, ResourceEnum } from '../types'
 
 export const chapel = (param = '') => {
   const { penny = 0, whiskey = 0, beer = 0 } = parseResourceParam(param)
@@ -14,3 +16,21 @@ export const chapel = (param = '') => {
     )
   )
 }
+
+export const complete = curry((partial: string[], state: GameStatePlaying): string[] =>
+  match(partial)
+    .with([], () => {
+      const { beer = 0, whiskey = 0, penny = 0 } = view(activeLens(state), state)
+      const rqIter = min(beer, whiskey)
+      return lift((a: string, b: string) => a + b)(
+        zipWith(
+          (a, b) => concat(a, b),
+          resourceArray(ResourceEnum.Beer, 3)(rqIter),
+          resourceArray(ResourceEnum.Whiskey, 3)(rqIter)
+        ),
+        resourceArray(ResourceEnum.Penny, 1)(penny)
+      )
+    })
+    .with([P._], always(['']))
+    .otherwise(always([]))
+)
