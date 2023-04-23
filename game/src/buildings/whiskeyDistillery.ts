d4eb7f2071da90bc71951789556afe82591630a4
Philihp Busby <philihp@gmail.com>
2023-04-23 07:40:24 +0000

    feature: add completions for USE whiskeyDistillery

--- game/src/buildings/whiskeyDistillery.ts
+++ game/src/buildings/whiskeyDistillery.ts
@@ -1,6 +1,8 @@
-import { pipe } from 'ramda'
-import { getCost, payCost, withActivePlayer } from '../board/player'
-import { parseResourceParam } from '../board/resource'
+import { always, curry, lift, map, min, pipe, props, range, view, zip, zipWith } from 'ramda'
+import { P, match } from 'ts-pattern'
+import { activeLens, getCost, payCost, withActivePlayer } from '../board/player'
+import { parseResourceParam, resourceArray, zip3 } from '../board/resource'
+import { Cost, GameStatePlaying, ResourceEnum, Tableau } from '../types'
 
 export const whiskeyDistillery = (input = '') => {
   const { malt = 0, wood = 0, peat = 0 } = parseResourceParam(input)
@@ -13,3 +15,19 @@ export const whiskeyDistillery = (input = '') => {
     )
   )
 }
+
+export const complete = curry((partial: string[], state: GameStatePlaying): string[] =>
+  match(partial)
+    .with([], () => {
+      const iterations = Math.min(
+        ...props<keyof Cost, number>(['malt', 'wood', 'peat'], view(activeLens(state), state))
+      )
+      return zip3(
+        resourceArray(ResourceEnum.Malt)(iterations),
+        resourceArray(ResourceEnum.Wood)(iterations),
+        resourceArray(ResourceEnum.Peat)(iterations)
+      )
+    })
+    .with([P._], always(['']))
+    .otherwise(always([]))
+)
