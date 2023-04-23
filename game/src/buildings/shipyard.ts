703192a0c8946162989e1970dd469a5685680b58
Philihp Busby <philihp@gmail.com>
2023-04-23 06:53:18 +0000

    feature: add completions for USE shipyard

--- game/src/buildings/shipyard.ts
+++ game/src/buildings/shipyard.ts
@@ -1,6 +1,8 @@
-import { pipe } from 'ramda'
-import { getCost, payCost, withActivePlayer } from '../board/player'
+import { always, curry, pipe, view } from 'ramda'
+import { P, match } from 'ts-pattern'
+import { activeLens, getCost, payCost, withActivePlayer } from '../board/player'
 import { parseResourceParam } from '../board/resource'
+import { GameStatePlaying } from '../types'
 
 export const shipyard = (param = '') => {
   const iterations = (parseResourceParam(param).wood ?? 0) / 2
@@ -12,3 +14,14 @@ export const shipyard = (param = '') => {
     )
   )
 }
+
+export const complete = curry((partial: string[], state: GameStatePlaying): string[] =>
+  match(partial)
+    .with([], () => {
+      const { wood = 0 } = view(activeLens(state), state)
+      if (wood >= 2) return ['WoWo', '']
+      return ['']
+    })
+    .with([P._], always(['']))
+    .otherwise(always([]))
+)
