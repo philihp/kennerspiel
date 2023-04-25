8ad37975bf7709c2688fb0bbf679be94937c12ef
Philihp Busby <philihp@gmail.com>
2023-04-25 09:20:39 +0000

    feature: add completions for USE inn

--- game/src/buildings/__tests__/inn.test.ts
+++ game/src/buildings/__tests__/inn.test.ts
@@ -8,77 +8,77 @@ import {
   Tableau,
   Tile,
 } from '../../types'
-import { inn } from '..'
+import { inn, complete } from '../inn'
 
 describe('buildings/inn', () => {
-  describe('inn', () => {
-    const p0: Tableau = {
-      color: PlayerColor.Blue,
-      clergy: [],
-      settlements: [],
-      landscape: [
-        [[], [], ['P'], ['P', 'LFO'], ['P', 'LFO'], ['P'], ['P'], [], []],
-        [[], [], ['P'], ['P', 'LFO'], ['P', 'LFO'], ['P'], ['P'], [], []],
-      ] as Tile[][],
-      wonders: 0,
-      landscapeOffset: 0,
-      peat: 0,
-      penny: 0,
-      clay: 0,
-      wood: 0,
-      grain: 10,
-      sheep: 0,
-      stone: 0,
-      flour: 0,
-      grape: 0,
-      nickel: 0,
-      malt: 0,
-      coal: 0,
-      book: 0,
-      ceramic: 0,
-      whiskey: 0,
-      straw: 0,
-      meat: 10,
-      ornament: 0,
-      bread: 0,
-      wine: 10,
-      beer: 0,
-      reliquary: 0,
-    }
-    const s0: GameStatePlaying = {
-      ...initialState,
-      status: GameStatusEnum.PLAYING,
-      frame: {
-        round: 1,
-        next: 1,
-        startingPlayer: 1,
-        settlementRound: SettlementRound.S,
-        currentPlayerIndex: 0,
-        activePlayerIndex: 0,
-        neutralBuildingPhase: false,
-        bonusRoundPlacement: false,
-        mainActionUsed: false,
-        bonusActions: [],
-        canBuyLandscape: true,
-        unusableBuildings: [],
-        usableBuildings: [],
-        nextUse: NextUseClergy.Any,
-      },
-      config: {
-        country: 'france',
-        players: 3,
-        length: 'long',
-      },
-      rondel: {
-        pointingBefore: 0,
-      },
-      wonders: 0,
-      players: [{ ...p0 }, { ...p0 }, { ...p0 }],
-      buildings: [],
-      plotPurchasePrices: [1, 1, 1, 1, 1, 1],
-      districtPurchasePrices: [],
-    }
+  const p0: Tableau = {
+    color: PlayerColor.Blue,
+    clergy: [],
+    settlements: [],
+    landscape: [
+      [[], [], ['P'], ['P', 'LFO'], ['P', 'LFO'], ['P'], ['P'], [], []],
+      [[], [], ['P'], ['P', 'LFO'], ['P', 'LFO'], ['P'], ['P'], [], []],
+    ] as Tile[][],
+    wonders: 0,
+    landscapeOffset: 0,
+    peat: 0,
+    penny: 0,
+    clay: 0,
+    wood: 0,
+    grain: 10,
+    sheep: 0,
+    stone: 0,
+    flour: 0,
+    grape: 0,
+    nickel: 0,
+    malt: 0,
+    coal: 0,
+    book: 0,
+    ceramic: 0,
+    whiskey: 0,
+    straw: 0,
+    meat: 10,
+    ornament: 0,
+    bread: 0,
+    wine: 10,
+    beer: 0,
+    reliquary: 0,
+  }
+  const s0: GameStatePlaying = {
+    ...initialState,
+    status: GameStatusEnum.PLAYING,
+    frame: {
+      round: 1,
+      next: 1,
+      startingPlayer: 1,
+      settlementRound: SettlementRound.S,
+      currentPlayerIndex: 0,
+      activePlayerIndex: 0,
+      neutralBuildingPhase: false,
+      bonusRoundPlacement: false,
+      mainActionUsed: false,
+      bonusActions: [],
+      canBuyLandscape: true,
+      unusableBuildings: [],
+      usableBuildings: [],
+      nextUse: NextUseClergy.Any,
+    },
+    config: {
+      country: 'france',
+      players: 3,
+      length: 'long',
+    },
+    rondel: {
+      pointingBefore: 0,
+    },
+    wonders: 0,
+    players: [{ ...p0 }, { ...p0 }, { ...p0 }],
+    buildings: [],
+    plotPurchasePrices: [1, 1, 1, 1, 1, 1],
+    districtPurchasePrices: [],
+  }
 
+  describe('inn', () => {
     it('goes through a happy path', () => {
       const s1 = inn('WnMtGnGnGn')(s0)! as GameStatePlaying
       expect(s1.players[0]).toMatchObject({
@@ -132,6 +132,135 @@ describe('buildings/inn', () => {
         penny: 1,
         nickel: 0,
       })
+    })
+  })
+
+  describe('complete', () => {
+    it('if meat and wine...', () => {
+      const s1 = {
+        ...s0,
+        players: [
+          {
+            ...s0.players[0],
+            wine: 1,
+            meat: 2,
+            sheep: 0,
+            grain: 0,
+          },
+          ...s0.players.slice(1),
+        ],
+      }
+      const c0 = complete([])(s1)
+      expect(c0).toStrictEqual(['MtMtWn', 'MtMt', 'MtWn', 'Mt', 'Wn', ''])
+    })
+    it('gives all combinations of one food things', () => {
+      const s1 = {
+        ...s0,
+        players: [
+          {
+            ...s0.players[0],
+            wine: 2,
+            meat: 0,
+            sheep: 0,
+            grain: 2,
+            penny: 2,
+          },
+          ...s0.players.slice(1),
+        ],
+      }
+      const c0 = complete([])(s1)
+      expect(c0).toStrictEqual([
+        'GnGnPnPnWnWn',
+        'GnPnPnWnWn',
+        'GnGnPnWnWn',
+        'GnGnPnPnWn',
+        'GnGnPnPn',
+        'PnPnWnWn',
+        'GnPnWnWn',
+        'GnPnPnWn',
+        'GnGnWnWn',
+        'GnGnPnWn',
+        'GnPnPn',
+        'GnGnPn',
+        'PnWnWn',
+        'PnPnWn',
+        'GnWnWn',
+        'GnPnWn',
+        'GnGnWn',
+        'GnGn',
+        'PnPn',
+        'GnPn',
+        'WnWn',
+        'PnWn',
+        'GnWn',
+        'Gn',
+        'Pn',
+        'Wn',
+        '',
+      ])
+    })
+    it('multiple types to make food', () => {
+      const s1 = {
+        ...s0,
+        players: [
+          {
+            ...s0.players[0],
+            wine: 1,
+            meat: 2,
+            sheep: 4,
+            grain: 1,
+            penny: 1,
+          },
+          ...s0.players.slice(1),
+        ],
+      }
+      const c0 = complete([])(s1)
+      expect(c0).toStrictEqual([
+        'MtMtWn',
+        'MtMt',
+        'ShShShShWn',
+        'ShShShSh',
+        'MtShWn',
+        'MtSh',
+        'ShShShGnWn',
+        'ShShShGn',
+        'ShShShPnWn',
+        'ShShShPn',
+        'MtGnPnWn',
+        'MtGnPn',
+        'ShShGnPnWn',
+        'ShShShWn',
+        'MtPnWn',
+        'MtGnWn',
+        'ShShSh',
+        'MtGn',
+        'ShShGnPn',
+        'MtPn',
+        'ShShPnWn',
+        'ShShGnWn',
+        'MtWn',
+        'Mt',
+        'ShShGn',
+        'ShShPn',
+        'ShGnPnWn',
+        'ShShWn',
+        'ShSh',
+        'ShGnPn',
+        'ShPnWn',
+        'ShGnWn',
+        'ShGn',
+        'ShPn',
+        'GnPnWn',
+        'ShWn',
+        'Sh',
+        'GnPn',
+        'PnWn',
+        'GnWn',
+        'Gn',
+        'Pn',
+        'Wn',
+        '',
+      ])
     })
   })
 })
