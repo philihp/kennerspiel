4478f714af15b36077851a44c5e1e34dab4e2b37
Philihp Busby <philihp@gmail.com>
2023-04-26 01:55:17 +0000

    feature: add completions for USE roundTower

--- game/src/buildings/roundTower.ts
+++ game/src/buildings/roundTower.ts
@@ -1,9 +1,9 @@
-import { always, curry, identity, pipe } from 'ramda'
+import { always, curry, identity, map, pipe, unnest, view } from 'ramda'
 import { P, match } from 'ts-pattern'
-import { getWonder, payCost, withActivePlayer } from '../board/player'
-import { costPoints, parseResourceParam } from '../board/resource'
+import { activeLens, getWonder, payCost, withActivePlayer } from '../board/player'
+import { coinCostOptions, costPoints, parseResourceParam, pointCostOptions } from '../board/resource'
 import { removeWonder } from '../board/state'
-import { CostReducer, GameStatePlaying } from '../types'
+import { CostReducer, GameStatePlaying, ResourceEnum } from '../types'
 
 const removeWhiskey: CostReducer = (cost) => {
   if (!cost) return undefined
@@ -51,7 +51,28 @@ export const roundTower = (param = '') => {
 
 export const complete = curry((partial: string[], state: GameStatePlaying): string[] =>
   match(partial)
-    .with([], always([]))
+    .with([], () => {
+      const player = view(activeLens(state), state)
+      if (!player.whiskey) return ['']
+
+      // 1st: just remove the whiskey
+      const paidWhiskey = payCost({ whiskey: 1 })(player)
+
+      // 2nd: find all the ways, without that whiskey, to pay 5 coins
+      const waysToPayCoins = coinCostOptions(5)(paidWhiskey)
+
+      // 3rd: with each of those ways, find all the ways to make 14 points
+      const waysToMakePoints = map((wayToPayCoin: string): string[] => {
+        const paidCoins = payCost(parseResourceParam(wayToPayCoin))(paidWhiskey)
+        const waysToPayPoints = pointCostOptions(14)(paidCoins)
+        return map(
+          (wayToPayPoints: string): string => `${wayToPayCoin}${ResourceEnum.Whiskey}${wayToPayPoints}`,
+          waysToPayPoints
+        )
+      }, waysToPayCoins)
+
+      return [...unnest(waysToMakePoints), '']
+    })
     .with([P._], always(['']))
     .otherwise(always([]))
 )
