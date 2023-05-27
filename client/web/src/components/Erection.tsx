7a1974bafffb3dc854cca1ba2904a1e9a5f1b225
‮Philihp Busby <philihp@gmail.com>
2023-05-27 07:08:31 +0000

    feature: web farmyard has prompt dialog (#329)
    * farmyard wip
    
    * feature: farmyard pops up next to button

--- client/web/src/components/Erection.tsx
+++ client/web/src/components/Erection.tsx
@@ -1,3 +1,7 @@
+import { ChangeEvent } from 'react'
+import classes from './Erection.module.css'
+import { useHathoraContext } from '../context/GameContext'
+
 interface Props {
   id: string
   disabled?: boolean
@@ -12,24 +16,46 @@ const decolor = (id: string) => {
   return id
 }
 
+const isFarmYard = (id: string) => decolor(id) === 'L02'
+
 const multiplier = 0.4
 
-export const Erection = ({ id, disabled, onClick }: Props) => (
-  <div style={{ display: 'inline-block' }}>
-    <img
-      alt={id}
-      title={id}
-      style={{ display: 'inline' }}
-      src={`https://hathora-et-labora.s3-us-west-2.amazonaws.com/${decolor(id)}.jpg`}
-      width={150 * multiplier}
-      height={250 * multiplier}
-    />
-    <br />
-    <button type="button" onClick={onClick} disabled={disabled}>
-      {id}
-    </button>
-  </div>
-)
+export const Erection = ({ id, disabled, onClick }: Props) => {
+  const { state, control } = useHathoraContext()
+  const onFarmYardClick = (e: ChangeEvent<HTMLSelectElement>) => {
+    if (e.target.value === ' ') return
+    console.log({ target: e.target.value, control: `${state?.control?.partial} ${e.target.value}` })
+    control(`${state?.control?.partial} ${e.target.value}`)
+  }
+
+  return (
+    <div style={{ display: 'inline-block' }}>
+      <img
+        alt={id}
+        style={{ display: 'inline' }}
+        src={`https://hathora-et-labora.s3-us-west-2.amazonaws.com/${decolor(id)}.jpg`}
+        width={150 * multiplier}
+        height={250 * multiplier}
+      />
+      <br />
+
+      {isFarmYard(id) && state?.control?.partial?.endsWith(`${id}`) && (
+        <div className={classes.dialog}>
+          <select onChange={onFarmYardClick}>
+            <option> </option>
+            <option>Gn</option>
+            <option>Sh</option>
+            <option>JoGn</option>
+            <option>JoSh</option>
+          </select>
+        </div>
+      )}
+      <button type="button" onClick={onClick} disabled={disabled}>
+        {id}
+      </button>
+    </div>
+  )
+}
 
 Erection.defaultProps = {
   disabled: true,
