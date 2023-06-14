80af68bfda5ded9e148acd4b49664711a70a4174
Philihp Busby <philihp@gmail.com>
2023-06-14 03:36:18 +0000

    fix: stoneMerchant does a max 5 iterations

--- game/src/buildings/stoneMerchant.ts
+++ game/src/buildings/stoneMerchant.ts
@@ -1,4 +1,4 @@
-import { always, curry, map, min, pipe, range, reverse, unnest, view } from 'ramda'
+import { always, curry, map, max, min, pipe, range, reverse, unnest, view } from 'ramda'
 import { P, match } from 'ts-pattern'
 import { getCost, withActivePlayer, payCost, activeLens } from '../board/player'
 import { costEnergy, costFood, parseResourceParam, settlementCostOptions } from '../board/resource'
@@ -22,7 +22,7 @@ export const complete = curry((partial: string[], state: GameStatePlaying): stri
       const player = view(activeLens(state), state)
       const food = Math.floor(costFood(player) / 2)
       const energy = costEnergy(player)
-      const maxIterations = min(food, energy)
+      const maxIterations = min(5, min(food, energy))
       const iterations = reverse(range(0, 1 + maxIterations)) // [ 0, 1, 2, 3, 4, 5 ]
       return unnest(map((i) => settlementCostOptions({ food: i * 2, energy: i }, player), iterations))
     })
