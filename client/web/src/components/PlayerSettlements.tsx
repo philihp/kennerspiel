32a21970dc13e4242b8cb150e4e8ea12fc228f40
‮Philihp Busby <philihp@gmail.com>
2023-10-04 16:14:10 +0000

    feature: ghosted settlements that will come later (#596)

--- client/web/src/components/PlayerSettlements.tsx
+++ client/web/src/components/PlayerSettlements.tsx
@@ -3,10 +3,27 @@ import { Erection } from './Erection'
 
 interface Props {
   settlements: string[]
+  color: string
 }
 
-export const PlayerSettlements = ({ settlements }: Props) => {
+export const PlayerSettlements = ({ settlements, color }: Props) => {
   const { state, control } = useHathoraContext()
+  const showS05 =
+    state?.frame?.settlementRound &&
+    (['S'].includes(state?.frame?.settlementRound) ||
+      (state?.frame?.settlementRound === 'A' && state?.flow?.[0]?.settle))
+  const showS06 =
+    state?.frame?.settlementRound &&
+    (['S', 'A'].includes(state?.frame?.settlementRound) ||
+      (state?.frame?.settlementRound === 'B' && state?.flow?.[0]?.settle))
+  const showS07 =
+    state?.frame?.settlementRound &&
+    (['S', 'A', 'B'].includes(state?.frame?.settlementRound) ||
+      (state?.frame?.settlementRound === 'C' && state?.flow?.[0]?.settle))
+  const showS08 =
+    state?.frame?.settlementRound &&
+    (['S', 'A', 'B', 'C'].includes(state?.frame?.settlementRound) ||
+      (state?.frame?.settlementRound === 'D' && state?.flow?.[0]?.settle))
   return (
     <div style={{ margin: 10 }}>
       {settlements.map((settlement) => {
@@ -18,6 +35,10 @@ export const PlayerSettlements = ({ settlements }: Props) => {
         }
         return <Erection key={settlement} id={settlement} onClick={handleClick} disabled={disabled} />
       })}
+      {showS05 && <Erection key={`S${color}5`} id={`S${color}5`} onClick={() => {}} disabled ghosted />}
+      {showS06 && <Erection key={`S${color}6`} id={`S${color}6`} onClick={() => {}} disabled ghosted />}
+      {showS07 && <Erection key={`S${color}7`} id={`S${color}7`} onClick={() => {}} disabled ghosted />}
+      {showS08 && <Erection key={`S${color}8`} id={`S${color}8`} onClick={() => {}} disabled ghosted />}
     </div>
   )
 }
