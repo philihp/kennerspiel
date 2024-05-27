15ebc6cdd3717ec1604f4c609921e9e11ae6c7e8
Philihp Busby <philihp@gmail.com>
2024-05-27 07:03:29 +0000

    remove character

--- web/src/app/character/actions.ts
+++
@@ -1,9 +0,0 @@
-'use server'
-
-import { redirect } from 'next/navigation'
-
-import { sso, scopes } from './sso'
-
-export const register = async (formData: FormData) => {
-  redirect(sso.getRedirectUrl('state', scopes))
-}
