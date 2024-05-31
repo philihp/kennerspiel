f491275e4a9771abae93e3c735f57bbdb5f0de93
‮Philihp Busby <philihp@gmail.com>
2024-05-31 07:02:33 +0000

    feature: create page for instances (#825)

--- web/src/app/layout/header.tsx
+++ web/src/app/layout/header.tsx
@@ -1,33 +1,38 @@
-import Link from "next/link"
-import { cookies } from 'next/headers';
-import { createClient } from '@/utils/supabase/server';
+import Link from 'next/link'
+import { cookies } from 'next/headers'
+import { createClient } from '@/utils/supabase/server'
 
 const Header = async () => {
   const cookieStore = cookies()
   const supabase = createClient(cookieStore)
 
-  const { data: { user } } = await supabase.auth.getUser()
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
   const userId = user?.email ?? undefined
-  return <header>
-
-    Kennerspiel
-    {!userId &&
-      <>
-        &nbsp;[&nbsp;
-        <Link href="/account/login">Login</Link>
-        &nbsp;|&nbsp;
-        <Link href="/account/register">Register</Link>
-        &nbsp;]
-      </>}
-    {userId &&
-      <>
-        &nbsp;[&nbsp;
-        {userId}
-        &nbsp;|&nbsp;
-        <Link href="/instances/">Instances</Link>
-        &nbsp;|&nbsp;
-        <Link href="/account/settings">Settings</Link> ]
-      </>}
-  </header>
+  return (
+    <header>
+      Kennerspiel
+      {!userId && (
+        <>
+          &nbsp;[&nbsp;
+          <Link href="/account/login">Login</Link>
+          &nbsp;|&nbsp;
+          <Link href="/account/register">Register</Link>
+          &nbsp;]
+        </>
+      )}
+      {userId && (
+        <>
+          &nbsp;[&nbsp;
+          {userId}
+          &nbsp;|&nbsp;
+          <Link href="/instance/">Instance</Link>
+          &nbsp;|&nbsp;
+          <Link href="/account/settings">Settings</Link> ]
+        </>
+      )}
+    </header>
+  )
 }
 export default Header
