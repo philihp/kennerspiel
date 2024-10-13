1621583e899ca6a497c5d1588e057d7cb13a1e8c
‮Philihp Busby <philihp@gmail.com>
2024-10-13 21:20:30 +0000

    feature: create instance button (#1001)
    * lint-staged refactor
    
    * create instance and refresh
    
    * fix: useCallback, how to?

--- web/src/app/instance/actions.ts
+++ web/src/app/instance/actions.ts
@@ -1,12 +1,11 @@
+'use server'
+
 import { createClient } from '@/utils/supabase/server'
-import { PostgrestError } from '@supabase/supabase-js'
 
 export const createInstance = async (formData: FormData) => {
-  'use server'
-
-  console.log(`creating ${JSON.stringify(formData)}`)
   const supabase = createClient()
-
   const { data, error } = await supabase.from('instance').insert([{}]).select()
-  return { message: JSON.stringify(data), error }
+  if (error === null) {
+    console.log({ redirect: `/instance/${data?.[0]?.id}` })
+  }
 }
