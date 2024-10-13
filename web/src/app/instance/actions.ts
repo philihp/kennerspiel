72a15a7dd9ab744d8e4f747c5a7f07acf1e536c4
‮Philihp Busby <philihp@gmail.com>
2024-10-13 22:24:46 +0000

    refactor: move create instance form stuff inside of button (#1002)
    * lint-staged refactor
    
    * encapsulate action inside create button

--- web/src/app/instance/actions.ts
+++ web/src/app/instance/actions.ts
@@ -1,11 +1,16 @@
 'use server'
 
 import { createClient } from '@/utils/supabase/server'
+import { PostgrestError } from '@supabase/supabase-js'
 
-export const createInstance = async (formData: FormData) => {
+export type FormState = {
+  error?: PostgrestError
+  id?: string
+}
+
+export const createInstance = async (prevState: FormState, formData: FormData): Promise<FormState> => {
   const supabase = createClient()
   const { data, error } = await supabase.from('instance').insert([{}]).select()
-  if (error === null) {
-    console.log({ redirect: `/instance/${data?.[0]?.id}` })
-  }
+  if (error) return { error }
+  return { id: data?.[0]?.id }
 }
