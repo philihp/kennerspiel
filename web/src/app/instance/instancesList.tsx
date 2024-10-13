49661f5236c6e60130f85795af6fd3d1b66b0c48
Philihp Busby <philihp@gmail.com>
2024-10-13 20:55:50 +0000

    create instance and refresh

--- web/src/app/instance/instancesList.tsx
+++ web/src/app/instance/instancesList.tsx
@@ -1,24 +1,40 @@
 'use client'
 
+import { createClient } from "@/utils/supabase/client"
+import { useEffect, useState } from "react"
+
 type Instance = {
   id: string
   created_at: string
   commands: string[]
 }
 
-export const InstancesList = ({ instances }: { instances: Instance[] }) => {
+export const InstancesList = () => {
+  const supabase = createClient()
+  const [instances, setInstances] = useState<Instance[] | undefined>(undefined)
+
+  const handleLoad = () => {
+    supabase.from('instance').select().then(({ data }) => {
+      setInstances(data ?? [] as Instance[])
+    })
+  }
+
+  useEffect(() => {
+    handleLoad()
+  }, [handleLoad])
+
   return (
     <>
+      <button onClick={handleLoad} type="button">Refresh</button>
       <ul>
-        {instances.map((instance) => (
+        {instances === undefined && <li><i>...</i></li>}
+        {instances !== undefined && instances.map((instance) => (
           <li key={instance.id}>
             <a href={`instance/${instance.id}`}>{instance.id}</a>
           </li>
         ))}
-        {instances.length === 0 && (
-          <li>
-            <i>(none)</i>
-          </li>
+        {instances?.length === 0 && (
+          <li><i>(none)</i></li>
         )}
       </ul>
     </>
--- web/src/app/instance/page.tsx
+++ web/src/app/instance/page.tsx
@@ -1,27 +1,15 @@
-import { createClient } from '@/utils/supabase/server'
+'use client'
+
 import { CreateButton } from './createButton'
 import { InstancesList } from './instancesList'
-import { revalidatePath } from 'next/cache'
-import { redirect } from 'next/navigation'
+import { createInstance } from './actions'
 
-const InstancePage = async () => {
-  const supabase = createClient()
-  const { data, error } = await supabase.from('instance').select()
-
-  const createInstance = async (formData: FormData) => {
-    'use server'
-    const supabase = createClient()
-    const { data, error } = await supabase.from('instance').insert([{}]).select()
-    const id = data?.[0]?.id
-    if (error === undefined) redirect(`/instance/${id}`)
-  }
-
+const InstancePage = () => {
   return (
     <>
       <section>
-        <b>{error && JSON.stringify(error)}</b>
+        {<InstancesList />}
       </section>
-      <section>{data && <InstancesList instances={data ?? []} />}</section>
       <section>
         <form action={createInstance}>
           <CreateButton />
