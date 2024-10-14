8bbab0737a4983a3e54dfb668450abc6de2e9766
Philihp Busby <philihp@gmail.com>
2024-10-14 05:03:14 +0000

    disable create instance if not logged in

--- web/src/app/instance/createButton.tsx
+++ web/src/app/instance/createButton.tsx
@@ -2,19 +2,41 @@
 
 import { useFormState, useFormStatus } from 'react-dom'
 import { createInstance, FormState } from './actions'
-import { redirect } from 'next/navigation'
+import { redirect, RedirectType } from 'next/navigation'
+import { User } from '@supabase/supabase-js'
+import { useEffect, useState } from 'react'
 
-export const CreateButton = () => {
-  const [state, formAction] = useFormState(createInstance, {} as FormState)
-  const { pending } = useFormStatus()
-  if (state?.id) {
-    redirect(`/instance/${state?.id}`)
+type Params = {
+  user: User | null
+}
+
+export const CreateButton = ({ user }: Params) => {
+  const [{ error, id }, formAction, pending] = useFormState(createInstance, {} as FormState)
+  const [color, setColor] = useState('#000000')
+  const [message, setMessage] = useState('')
+
+  useEffect(() => {
+    if (!error) return
+    setMessage(error.message)
+    setColor('#FF0000')
+  }, [error])
+
+  if (id) {
+    redirect(`/instance/${id}`, RedirectType.push)
   }
+
   return (
     <form action={formAction}>
-      <button type="submit" aria-disabled={pending}>
+      <button type="submit" disabled={(user === null) || pending}>
         Create Instance
       </button>
+      {error && <>
+        <svg height="10" width="20">
+          <circle cx="10" cy="5" r="5" fill={color} />
+        </svg>
+        {message}
+      </>
+      }
     </form>
   )
 }
--- web/src/app/instance/page.tsx
+++ web/src/app/instance/page.tsx
@@ -1,16 +1,19 @@
 'use server'
 
+import { createClient } from '@/utils/supabase/server'
 import { CreateButton } from './createButton'
 import { InstancesList } from './instancesList'
 
 const InstancePage = async () => {
+  const supabase = createClient()
+  const { data: { user } } = await supabase.auth.getUser()
   return (
     <>
       <section>
         <InstancesList />
       </section>
       <section>
-        <CreateButton />
+        <CreateButton user={user} />
       </section>
     </>
   )
