26e13dbf4c4aab6daaf11b97c34db654bab48ee2
Philihp Busby <philihp@gmail.com>
2025-02-16 05:44:01 +0000

    build: fix lint-staged, bump eslint

--- web/lint-staged.config.js
+++ web/lint-staged.config.js
@@ -1,7 +1,9 @@
+import path from 'path'
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`
+
 export default {
-  "**/*.{json,html}": ["prettier --write"],
-  "**/*.{ts,tsx,js,jsx,json}": [
-    "prettier --write",
-    "eslint --fix",
-  ]
+  '**/*.{json,html}': ['prettier --write'],
+  '**/*.{ts,tsx,js,jsx}': ['prettier --write', buildEslintCommand],
 }
