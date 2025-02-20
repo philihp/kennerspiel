90496023bf1afbcb76e147c76daf83d0a8fc67bb
Philihp Busby <philihp@gmail.com>
2025-02-20 23:46:01 +0000

    feature: remove realtime table updates

---
+++ supabase/migrations/20250220234413_remove_realtime_table_updates.sql
@@ -0,0 +1,3 @@
+alter publication supabase_realtime drop table public.entrant;
+alter publication supabase_realtime drop table public.instance;
+alter publication supabase_realtime drop table public.profile;
