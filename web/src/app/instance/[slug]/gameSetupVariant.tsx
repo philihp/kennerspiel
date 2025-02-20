3e8e5c611efa9272c99957534f70cd04eef84b1c
Philihp Busby <philihp@gmail.com>
2025-02-20 22:15:34 +0000

    refactor: cleanup gameSetupVariant repeated code

--- web/src/app/instance/[slug]/gameSetupVariant.tsx
+++ web/src/app/instance/[slug]/gameSetupVariant.tsx
@@ -2,12 +2,29 @@ import { useInstanceContext } from '@/context/InstanceContext'
 import { EngineCountry, EngineLength } from '@/types'
 import { config } from './actions'
 import { Tables } from '@/supabase.types'
-import { useOptimistic, useTransition } from 'react'
+import { EventHandler, ReactNode, SyntheticEvent, useOptimistic, useTransition } from 'react'
 
 type Instance = Tables<'instance'>
 
 const configured = (country: EngineCountry, length: EngineLength, firstCommand: string = '') =>
   firstCommand.includes(country.toLowerCase()) && firstCommand.includes(length.toLowerCase())
+
+type RadioPairProps = {
+  disabled: boolean
+  onClick: EventHandler<SyntheticEvent<HTMLButtonElement | HTMLInputElement>> // is it even worth it?
+  children: ReactNode | ReactNode[]
+}
+
+const RadioPair = ({ disabled, onClick, children }: RadioPairProps) => {
+  return (
+    <>
+      <input type="radio" disabled={disabled} checked={disabled} onChange={onClick} />
+      <button type="button" disabled={disabled} onClick={onClick}>
+        {children}
+      </button>
+    </>
+  )
+}
 
 export const GameSetupVariant = () => {
   const { instance, setInstance, entrants } = useInstanceContext()
@@ -15,13 +32,10 @@ export const GameSetupVariant = () => {
   const [optInstance, setOptInstance] = useOptimistic<Instance, [EngineCountry, EngineLength]>(
     instance,
     // optimistically, predict what the resulting list of entrants will look like
-    (oldInstance, [country, length]) => {
-      console.log([`CONFIG ${entrants.length} ${country.toLowerCase()} ${length.toLowerCase()}`])
-      return {
-        ...oldInstance,
-        commands: [`CONFIG ${entrants.length} ${country.toLowerCase()} ${length.toLowerCase()}`],
-      }
-    }
+    (oldInstance, [country, length]) => ({
+      ...oldInstance,
+      commands: [`CONFIG ${entrants.length} ${country.toLowerCase()} ${length.toLowerCase()}`],
+    })
   )
 
   const [_isPending, startTransition] = useTransition()
@@ -29,80 +43,55 @@ export const GameSetupVariant = () => {
   const handleConfig = async (players: number, country: EngineCountry, length: EngineLength) => {
     startTransition(async () => {
       // optimistic reducer
-      console.log('optimistic')
       setOptInstance([country, length])
       const updatedInstance = await config(
         instance.id,
         `CONFIG ${Math.max(players, 1)} ${country.toLowerCase()} ${length.toLowerCase()}`
       )
-      console.log('pessimistic')
       // pessimistic reducer
       if (updatedInstance) setInstance(updatedInstance)
     })
   }
 
+  const isFranceLong = configured(EngineCountry.france, EngineLength.long, optInstance?.commands?.[0])
+  const isFranceShort = configured(EngineCountry.france, EngineLength.short, optInstance?.commands?.[0])
+  const isIrelandLong = configured(EngineCountry.ireland, EngineLength.long, optInstance?.commands?.[0])
+  const isIrelandShort = configured(EngineCountry.ireland, EngineLength.short, optInstance?.commands?.[0])
+
   return (
     <>
       <hr />
       <h3>Mode</h3>
-      <input
-        type="radio"
-        disabled={configured(EngineCountry.france, EngineLength.long, optInstance?.commands?.[0])}
-        checked={configured(EngineCountry.france, EngineLength.long, optInstance?.commands?.[0])}
-        onChange={() => handleConfig(entrants.length, EngineCountry.france, EngineLength.long)}
-      />
-      <button
-        type="button"
-        disabled={configured(EngineCountry.france, EngineLength.long, optInstance?.commands?.[0])}
+      <RadioPair
+        disabled={isFranceLong}
         onClick={() => handleConfig(entrants.length, EngineCountry.france, EngineLength.long)}
       >
         France {entrants?.length === 2 && '(long)'}
-      </button>
+      </RadioPair>
       <br />
-      <input
-        type="radio"
-        disabled={configured(EngineCountry.ireland, EngineLength.long, optInstance?.commands?.[0])}
-        checked={configured(EngineCountry.ireland, EngineLength.long, optInstance?.commands?.[0])}
-        onChange={() => handleConfig(entrants.length, EngineCountry.ireland, EngineLength.long)}
-      />
-      <button
-        type="button"
-        disabled={configured(EngineCountry.ireland, EngineLength.long, optInstance?.commands?.[0])}
+      <RadioPair
+        disabled={isIrelandLong}
         onClick={() => handleConfig(entrants.length, EngineCountry.ireland, EngineLength.long)}
       >
         Ireland {entrants?.length === 2 && '(long)'}
-      </button>
+      </RadioPair>
       <br />
       {entrants.length > 1 && (
         <>
           {' '}
-          <input
-            type="radio"
-            disabled={configured(EngineCountry.france, EngineLength.short, optInstance?.commands?.[0])}
-            checked={configured(EngineCountry.france, EngineLength.short, optInstance?.commands?.[0])}
-            onChange={() => handleConfig(entrants.length, EngineCountry.france, EngineLength.short)}
-          />
-          <button
-            type="button"
-            disabled={configured(EngineCountry.france, EngineLength.short, optInstance?.commands?.[0])}
+          <RadioPair
+            disabled={isFranceShort}
             onClick={() => handleConfig(entrants.length, EngineCountry.france, EngineLength.short)}
           >
             France {entrants?.length >= 3 && '(short)'}
-          </button>
+          </RadioPair>
           <br />
-          <input
-            type="radio"
-            disabled={configured(EngineCountry.ireland, EngineLength.short, optInstance?.commands?.[0])}
-            checked={configured(EngineCountry.ireland, EngineLength.short, optInstance?.commands?.[0])}
-            onChange={() => handleConfig(entrants.length, EngineCountry.ireland, EngineLength.long)}
-          />
-          <button
-            type="button"
-            disabled={configured(EngineCountry.ireland, EngineLength.short, optInstance?.commands?.[0])}
-            onClick={() => handleConfig(entrants.length, EngineCountry.ireland, EngineLength.long)}
+          <RadioPair
+            disabled={isIrelandShort}
+            onClick={() => handleConfig(entrants.length, EngineCountry.ireland, EngineLength.short)}
           >
             Ireland {entrants?.length >= 3 && '(short)'}
-          </button>
+          </RadioPair>
           <br />
         </>
       )}
