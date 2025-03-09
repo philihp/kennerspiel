1ccaf3d184cd8258aad9a65484c0529e350d292d
‮Philihp Busby <philihp@gmail.com>
2025-03-09 12:01:01 +0000

    feature: styling of rondel (#1181)

--- web/src/components/rondel.tsx
+++ web/src/components/rondel.tsx
@@ -1,8 +1,12 @@
+import { match } from 'ts-pattern'
 import { useInstanceContext } from '@/context/InstanceContext'
 import { addIndex, map, range, toPairs } from 'ramda'
 import { ReactNode } from 'react'
 import { RondelSettlements } from './rondel/settlements'
-import { mask, wedge, arrowPath, armPath } from './rondel/constants'
+import { mask, wedge, arrowPath, armPath, points } from './rondel/constants'
+import { GameCommandConfigParams } from 'hathora-et-labora-game'
+
+import styles from './rondel.module.css'
 
 const symbols = {
   wood: '🪵',
@@ -15,6 +19,39 @@ const symbols = {
   grape: '🍇',
   stone: '🪨',
 }
+
+const isStoneUsed = (config: GameCommandConfigParams): boolean =>
+  match(config)
+    .with({ players: 1 }, () => false)
+    .otherwise(() => true)
+
+const isGrapeUsed = (config: GameCommandConfigParams): boolean => {
+  return match(config)
+    .with({ players: 1 }, () => false)
+    .with({ country: 'france' }, () => false)
+    .otherwise(() => true)
+}
+
+const oraDeg = (pos?: number): number =>
+  match(pos)
+    .with(0, () => (360 * 12.5) / points.length)
+    .with(1, () => (360 * 11.5) / points.length)
+    .with(2, () => (360 * 10.5) / points.length)
+    .with(3, () => (360 * 9.5) / points.length)
+    .with(4, () => (360 * 8.5) / points.length)
+    .with(5, () => (360 * 7.5) / points.length)
+    .with(6, () => (360 * 6.5) / points.length)
+    .with(7, () => (360 * 5.5) / points.length)
+    .with(8, () => (360 * 4.5) / points.length)
+    .with(9, () => (360 * 3.5) / points.length)
+    .with(10, () => (360 * 2.5) / points.length)
+    .with(11, () => (360 * 1.5) / points.length)
+    .with(12, () => (360 * 0.5) / points.length)
+    .otherwise(() => 0)
+
+const BASE = -50
+
+const armOffset = (360 * 0.5) / points.length
 
 export const Rondel = () => {
   const { state, controls, addPartial } = useInstanceContext()
@@ -62,25 +99,64 @@ export const Rondel = () => {
 
         <RondelSettlements />
 
-        {/* <c:if test="${board.mode.grapesUsed}">
-          <g id="grape" transform="rotate(${ora:deg(board.wheel.grape.position)})">
-            <text x="0" y="${board.wheel.grape.radius}"
-            style="font-size: 9px; font-weight: 100; kerning:-0.5; text-anchor: middle; fill:#000">Grape</text>
+        {state?.rondel?.grape && state?.config && isGrapeUsed(state.config) && (
+          <g id="grape" transform={`rotate(${oraDeg(state?.rondel?.grape ?? 0)})`}>
+            <text x={50} y={BASE - 105} className={styles.token}>
+              {symbols.grape}
+            </text>
           </g>
-        </c:if>
-        <c:if test="${board.mode.stoneUsed}">
-          <g id="stone" transform="rotate(${ora:deg(board.wheel.stone.position)})">
-            <text x="0" y="${board.wheel.stone.radius}"
-            style="font-size: 9px; font-weight: 100; kerning:-0.5; text-anchor: middle; fill:#000">Stone</text>
+        )}
+
+        {state?.rondel?.stone && state?.config && isStoneUsed(state.config) && (
+          <g id="stone" transform={`rotate(${oraDeg(state?.rondel?.stone ?? 0)})`}>
+            <text x={0} y={BASE - 114} className={styles.token}>
+              {symbols.stone}
+            </text>
           </g>
-        </c:if> */}
+        )}
+
+        <g id="grain" transform={`rotate(${oraDeg(state?.rondel?.grain ?? 0)})`}>
+          <text x={0} y={BASE - 87} className={styles.token}>
+            {symbols.grain}
+          </text>
+        </g>
+        <g id="sheep" transform={`rotate(${oraDeg(state?.rondel?.sheep ?? 0)})`}>
+          <text x={0} y={BASE - 96} className={styles.token}>
+            {symbols.sheep}
+          </text>
+        </g>
+        <g id="joker" transform={`rotate(${oraDeg(state?.rondel?.joker ?? 0)})`}>
+          <text x={0} y={BASE - 78} className={styles.token}>
+            {symbols.joker}
+          </text>
+        </g>
+        <g id="wood" transform={`rotate(${oraDeg(state?.rondel?.wood ?? 0)})`}>
+          <text x={0} y={BASE - 69} className={styles.token}>
+            {symbols.wood}
+          </text>
+        </g>
+        <g id="clay" transform={`rotate(${oraDeg(state?.rondel?.clay ?? 0)})`}>
+          <text x={0} y={BASE - 60} className={styles.token}>
+            {symbols.clay}
+          </text>
+        </g>
+        <g id="peat" transform={`rotate(${oraDeg(state?.rondel?.peat ?? 0)})`}>
+          <text x={0} y={BASE - 51} className={styles.token}>
+            {symbols.peat}
+          </text>
+        </g>
+        <g id="coin" transform={`rotate(${oraDeg(state?.rondel?.coin ?? 0)})`}>
+          <text x={0} y={BASE - 42} className={styles.token}>
+            {symbols.coin}
+          </text>
+        </g>
 
         <g
           id="arm"
-          transform="rotate(${ora:deg(board.wheel.arm.position)-board.armOffset})"
+          transform={`rotate(${oraDeg(state?.rondel?.pointingBefore) - armOffset})`}
           style={{ fontSize: '10px', textAnchor: 'middle' }}
         >
-          <path d={armPath} style={{ fill: '#ffffff', fillOpacity: 1, stroke: '#686868', strokeWidth: 1 }} />
+          <path d={armPath} className={styles.armPath} />
           <path d={arrowPath} fill="#000" />
         </g>
       </svg>
