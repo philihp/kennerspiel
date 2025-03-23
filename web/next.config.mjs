8255d2fca80abb7cc7c9c844ed2acc3d0c5f2ad9
Philihp Busby <philihp@gmail.com>
2025-03-23 05:04:41 +0000

    feature: option to have builds linted

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: process.env.LINT_ON_BUILD,
   },
   images: {
     remotePatterns: [
