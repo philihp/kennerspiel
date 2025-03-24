3360ac513d0c17545a6e1bba7cf71fea31d53436
Philihp Busby <philihp@gmail.com>
2025-03-24 17:37:14 +0000

    fix: take will return 0 rather than undefined

--- web/src/components/rondel/values.ts
+++ web/src/components/rondel/values.ts
@@ -9,5 +9,5 @@ export const armValues = ({ length, players }: GameCommandConfigParams) => {
 
 export const take = (armIndex: number, tokenIndex: number, config: GameCommandConfigParams): number => {
   const armVals = armValues(config)
-  return armVals[(armIndex - tokenIndex + armVals.length) % armVals.length]
+  return armVals[(armIndex - tokenIndex + armVals.length) % armVals.length] ?? 0
 }
