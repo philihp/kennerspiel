f5cbdf3cac804829878c8a2bea4ae5f5cffa5640
Philihp Busby <philihp@gmail.com>
2025-03-31 00:40:01 +0000

    fix: cleaner way of making COMMIT secondary

--- web/src/components/sliders/actionCommit.tsx
+++ web/src/components/sliders/actionCommit.tsx
@@ -1,7 +1,7 @@
 import { useInstanceContext } from '@/context/InstanceContext'
 import classes from './actions.module.css'
 import { GameCommandEnum } from 'hathora-et-labora-game/dist/types'
-import { intersection, isEmpty } from 'ramda'
+import { intersection, isEmpty, isNotEmpty } from 'ramda'
 
 export const ActionCommit = () => {
   const { state, controls, addPartial, active } = useInstanceContext()
@@ -12,21 +12,20 @@ export const ActionCommit = () => {
 
   const disabled = !active || !(controls?.completion ?? []).includes('COMMIT')
 
-  const usableBonusAction = isEmpty(
-    intersection(state?.frame?.bonusActions ?? [], [
-      GameCommandEnum.CUT_PEAT,
-      GameCommandEnum.FELL_TREES,
-      GameCommandEnum.BUILD,
-      GameCommandEnum.USE,
-      GameCommandEnum.WORK_CONTRACT,
-      GameCommandEnum.SETTLE,
-    ])
-  )
-
   const maybeDont =
-    ((state?.frame?.bonusActions ?? []).length > 0 &&
-      !state?.frame?.bonusActions?.includes(GameCommandEnum.COMMIT) &&
-      usableBonusAction) ||
+    isNotEmpty(
+      intersection(
+        [
+          GameCommandEnum.CUT_PEAT,
+          GameCommandEnum.FELL_TREES,
+          GameCommandEnum.BUILD,
+          GameCommandEnum.USE,
+          GameCommandEnum.WORK_CONTRACT,
+          GameCommandEnum.SETTLE,
+        ],
+        state?.frame?.bonusActions ?? []
+      )
+    ) ||
     state?.frame?.usableBuildings?.length !== 0 ||
     state?.frame?.mainActionUsed !== true
 
@@ -40,20 +39,6 @@ export const ActionCommit = () => {
       >
         Commit
       </button>
-      <pre>
-        {JSON.stringify(
-          {
-            frame: state?.frame,
-            maybeDont,
-            t1a: (state?.frame?.bonusActions ?? []).length > 0,
-            t1b: !state?.frame?.bonusActions?.includes(GameCommandEnum.COMMIT),
-            t2: state?.frame?.usableBuildings?.length !== 0,
-            t3: state?.frame?.mainActionUsed !== true,
-          },
-          null,
-          2
-        )}
-      </pre>
     </>
   )
 }
