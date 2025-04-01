1dd1b1d2e35e9e35950b9e49954660e152aa68dc
‮Philihp Busby <philihp@gmail.com>
2025-04-01 00:11:01 +0000

    setup using the Flags SDK (#1236)

--- web/src/app/.well-known/vercel/flags/route.ts
+++ web/src/app/.well-known/vercel/flags/route.ts
@@ -1,20 +1,26 @@
+import { verifyAccess, type ApiData } from 'flags'
+import { getProviderData } from 'flags/next'
 import { NextResponse, type NextRequest } from 'next/server'
-import { verifyAccess, type ApiData } from '@vercel/flags'
+import * as flags from '../../../flags'
+
+//   return NextResponse.json<ApiData>({
+//     definitions: {
+//       ireland: {
+//         description: 'Allow Ireland Variant',
+//         origin: 'https://example.com/#new-feature',
+//         options: [
+//           { value: false, label: 'Locked' },
+//           { value: true, label: 'Unlocked' },
+//         ],
+//       },
+//     },
+//   })
+// }
 
 export async function GET(request: NextRequest) {
   const access = await verifyAccess(request.headers.get('Authorization'))
   if (!access) return NextResponse.json(null, { status: 401 })
 
-  return NextResponse.json<ApiData>({
-    definitions: {
-      pretzel: {
-        description: 'Pretzel',
-        origin: 'https://example.com/#new-feature',
-        options: [
-          { value: false, label: 'Off' },
-          { value: true, label: 'On' },
-        ],
-      },
-    },
-  })
+  const providerData = getProviderData(flags)
+  return NextResponse.json<ApiData>(providerData)
 }
